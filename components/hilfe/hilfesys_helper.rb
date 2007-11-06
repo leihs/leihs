f2c4b9a544a2b56739c4c376451d20185c5e958b
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2007-11-06 21:00:03 +0000

    more fixes to Prototype 1.5.0 incompatibility.
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@70 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- components/hilfe/hilfesys_helper.rb
+++ components/hilfe/hilfesys_helper.rb
@@ -8,11 +8,13 @@ module Hilfe::HilfesysHelper
 		idzu = in_id + '_zu'
 		idauf = in_id + '_auf'
 		html += '<div id="' + idzu + '" style="display:' + ( in_anfangs_offen ? 'none' : 'block' ) + ';"><h3 class="hilfe_einblender">'
-		html += link_to_function( image_tag( 'klapp_zu.png' ), "Element.toggle('#{idzu}', '#{idauf}')" )
-		html += '&nbsp;' + link_to_function( in_titel, "Element.toggle('#{idzu}', '#{idauf}')" ) + '</h3></div>'
+		html += link_to_function( image_tag( 'klapp_zu.png' ), "['#{idzu}', '#{idauf}'].each(Element.toggle)" )
+
+		html += '&nbsp;' + link_to_function( in_titel, "['#{idzu}', '#{idauf}'].each(Element.toggle)" ) + '</h3></div>'
 		html += '<div id="' + idauf + '" style="display:' + ( in_anfangs_offen ? 'block' : 'none' ) + ';"><h3 class="hilfe_einblender">'
-		html += link_to_function( image_tag( 'klapp_auf.png' ), "Element.toggle('#{idzu}', '#{idauf}')" )
-		html += '&nbsp;' + link_to_function( in_titel, "Element.toggle('#{idzu}', '#{idauf}')" ) + '</h3>'
+		html += link_to_function( image_tag( 'klapp_auf.png' ), "['#{idzu}', '#{idauf}'].each(Element.toggle)" )
+
+		html += '&nbsp;' + link_to_function( in_titel, "['#{idzu}', '#{idauf}'].each(Element.toggle)" ) + '</h3>'
 		return html
 	end
 	
