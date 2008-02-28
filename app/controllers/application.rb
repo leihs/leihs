075f0f942df912af2cf349d3447b328e7a7aaa40
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-02-28 08:31:08 +0000

    Version 2.x is now the trunk.
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@201 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'a51355e168a2870e8e42d11f9390b986'
+end
