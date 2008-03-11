0a8f3bf187c0648a727ae8b207c965fcae291123
jerome <jerome@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-03-11 19:56:18 +0000

    First 3 Specs are running.
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@207 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,22 @@
 class Order < ActiveRecord::Base
 
-  NEW = 'new'
-
   belongs_to :user
-  
+  has_many :order_lines
+
+  NEW = 'new'
+  APPROVED = "approved"
+
   def self.new_orders
     find(:all, :conditions => {:status => Order::NEW})
   end
 
+
+  def add(quantity, type)
+    o = OrderLine.new(:quantity => quantity, 
+                      :order_id => id, 
+                      :type_id => type.id)
+                  
+    order_lines << o
+  end
+  
 end
