53345a35cf062d7d40fe270be17c43f4ba8d7f22
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-04-01 15:34:21 +0000

    acts_as_commentable plugin for Order model
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@239 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,8 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_lines
 
+  acts_as_commentable
+
   NEW = 'new'
   APPROVED = "approved"
   REJECTED = "rejected"
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 16) do
+ActiveRecord::Schema.define(:version => 17) do
 
   create_table "access_rights", :force => true do |t|
     t.integer  "role_id"
@@ -42,6 +42,17 @@ ActiveRecord::Schema.define(:version => 16) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", :force => true do |t|
+    t.string   "title",            :limit => 50, :default => ""
+    t.string   "comment",                        :default => ""
+    t.datetime "created_at",                                     :null => false
+    t.integer  "commentable_id",                 :default => 0,  :null => false
+    t.string   "commentable_type", :limit => 15, :default => "", :null => false
+    t.integer  "user_id",                        :default => 0,  :null => false
+  end
+
+  add_index "comments", ["user_id"], :name => "fk_comments_user"
 
   create_table "contract_lines", :force => true do |t|
     t.integer  "contract_id"
