98a2cc791d924dd8ed1cc1a14f5058309478f6c6
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-04-01 08:59:30 +0000

    habtm tables
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@231 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- db/migrate/012_create_roles_and_roles_users.rb
+++ db/migrate/012_create_roles_and_roles_users.rb
@@ -5,14 +5,18 @@ class CreateRolesAndRolesUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    create_table :roles_users do |t|
+    create_table :roles_users, :id => false do |t|
       t.belongs_to :role
       t.belongs_to :user
     end
+    add_index(:roles_users, :role_id)
+    add_index(:roles_users, :user_id)
+
   
   end
 
   def self.down
     drop_table :roles
+    drop_table :roles_users
   end
 end
--- db/migrate/015_create_packages_and_models_packages.rb
+++ db/migrate/015_create_packages_and_models_packages.rb
@@ -5,14 +5,17 @@ class CreatePackagesAndModelsPackages < ActiveRecord::Migration
       t.timestamps
     end
     
-    create_table :models_packages do |t|
+    create_table :models_packages, :id => false do |t|
       t.belongs_to :model
       t.belongs_to :package
     end
+    add_index(:models_packages, :model_id)
+    add_index(:models_packages, :package_id)
 
   end
 
   def self.down
     drop_table :packages
+    drop_table :models_packages
   end
 end
