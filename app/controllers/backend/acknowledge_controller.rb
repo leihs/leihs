337e928ec63bd1fed3d8d4f112f5bd671e2404f6
jerome <jerome@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-04-01 09:05:42 +0000

    This stuff is work in progress. Proceed at your own risk ;-)
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@234 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/controllers/backend/acknowledge_controller.rb
+++ app/controllers/backend/acknowledge_controller.rb
@@ -1,5 +1,36 @@
 class Backend::AcknowledgeController < Backend::BackendController
 
+
+  def create_some_inventory
+    params[:id].to_i.times do |i|
+      m = Model.new(:name => params[:name] + " " + i.to_s)
+      m.save
+      5.times do |serial_nr|
+        i = Item.new(:model_id => m.id, :inventory_code => serial_nr)
+        
+        i.save        
+      end
+    end
+  end
+  
+  def create_some_users
+    params[:id].to_i.times do |i|
+      u = User.new(:login => "#{params[:name]}_#{i}")
+      u.save
+    end
+  end
+  
+  def create_some_new_orders
+    users = User.find(:all)
+    models = Model.find(:all)
+    params[:id].to_i.times do |i|
+      order = Order.new()
+      order.user_id = users[rand(users.size)].id
+      order.add(rand(3), models[rand(models.size)])
+      order.save
+    end
+  end
+  
   def index
   end
   
@@ -31,9 +62,23 @@ class Backend::AcknowledgeController < Backend::BackendController
   def change_line
     if request.post?
       @order_line = OrderLine.find(params[:id])
-      @order_line.quantity = params[:quantity].to_i
-      @success = @order_line.save
+      original = @order_line.quantity
+      required_quantity = params[:quantity].to_i
+      @order_line.quantity = required_quantity < max_available ? required_quantity : max_available
+      @change = "Quantity updated from #{original.to_s} to #{@order_line.quantity}"
+      
+      if required_quantity > max_available
+        @flash_notice = _("Maximum number of items available at that time is 9")
+        @change += " (maximum available)"
+      end
+      
+      @order_line.save
     end
   end
   
+  private
+  
+  def max_available
+    10
+  end
 end
