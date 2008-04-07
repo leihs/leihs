96fbb977af0de382e001845236b1fae3fb0a2d90
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-04-07 10:20:54 +0000

    popup and button helpers
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@252 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def link_button(text, options = {})
+    
+  end
+
+  def submit_button(text, options = {})
+    #TODO check options: img, class
+ 
+    '<a href="javascript://donothing" onclick="f.submit();"' +   
+       (options[:class].nil? ? '' : ' class="' + options[:class] + '">') +
+       (options[:img].nil? ? '' : image_tag(options[:img])) +
+       ' ' + text + '</a>'
+  end
+  
+  def cancel_popup_button(text, options = {})
+    link_to_function(text, "parent.parent.GB_hide();")
+  end
+
 end
