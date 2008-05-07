132bba3bd3639967f18914e9649267770ff94fa5
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-05-07 08:17:54 +0000

    hand over (working on the show: generating new contract and relative contract lines)
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@306 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/controllers/backend/hand_over_controller.rb
+++ app/controllers/backend/hand_over_controller.rb
@@ -1,33 +1,32 @@
 class Backend::HandOverController < Backend::BackendController
-  
+
   def index
-#      orders = Order.find(:all,
-#                           :conditions => {:status_const => Order::APPROVED},
-#                           :group => :user_id )
-      orders = Order.approved_orders
-                           
+#      orders = Order.approved_orders
 #      @users = orders.collect {|o| o.user }.uniq
 #      @order_lines = orders.collect {|o| o.order_lines }.flatten
-
-      @orders = orders #TODO temp
-
-
-      
-      
-      
 
       @grouped_lines = OrderLine.find_by_sql("SELECT u.id AS user_id,
                                                    u.login AS user_login,
                                                    sum(ol.quantity) AS quantity,
                                                    ol.start_date
-                                            FROM order_lines ol JOIN orders o ON ol.order_id = o.id
-                                                                JOIN users u ON o.user_id = u.id
-                                            WHERE o.status_const = 2
-                                            GROUP BY ol.start_date, u.id 
-                                            ORDER BY ol.start_date, u.id") 
+                                              FROM order_lines ol JOIN orders o ON ol.order_id = o.id
+                                                                  JOIN users u ON o.user_id = u.id
+                                              WHERE o.status_const = 2
+                                              GROUP BY ol.start_date, u.id 
+                                              ORDER BY ol.start_date, u.id") 
+  end
 
-
-
+  # generates a new contract and contract_lines for each item
+  def show
+    orders = User.find(params[:id]).orders.approved_orders
+    @order_lines = orders.collect {|o| o.order_lines }.flatten
+    # TODO sort by start_date
+    @contract = Contract.new # TODO if doesn't exist
+    @order_lines.each do |ol|
+      ol.quantity.times do
+        @contract.contract_lines << ContractLine.new #(:item => ol.model....)
+      end
+    end
   end
   
   
