dfa01a8970b03502a9efbece0390f3a451c305f5
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-05-13 16:41:10 +0000

    display working orders on the acknowledge index
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@318 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/controllers/backend/temporary_controller.rb
+++ app/controllers/backend/temporary_controller.rb
@@ -117,12 +117,12 @@ class Backend::TemporaryController < Backend::BackendController
   def clean_db_and_index
     Item.delete_all
     Model.delete_all
-    Order.destroy_all #destroy_all
+    Order.delete_all #destroy_all
     OrderLine.delete_all
     User.delete_all
     Backup::Order.delete_all #destroy_all
     Backup::OrderLine.delete_all
-    Contract.destroy_all
+    Contract.delete_all
     ContractLine.delete_all
       
     FileUtils.remove_dir(File.dirname(__FILE__) + "/../../../index", true)
