2f1b7ad47cbd24bf7f2457508c6b03008d024b58
jerome <jerome@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-06-30 11:58:34 +0000

    login_steps (duplication) removed
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@362 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- stories/hand_over_story.rb
+++ stories/hand_over_story.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + "/helper"
 
-with_steps_for(:hand_over) do
+with_steps_for(:hand_over, :login) do
   run_local_story "./text/hand_over_story", :type => RailsStory
 end
