da8bcfe7685e103249238f106e43d252adc705e5
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-10-22 10:46:02 +0000

    removed active_scaffold plugin
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@451 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/controllers/admin/models_controller.rb
+++ app/controllers/admin/models_controller.rb
@@ -2,15 +2,6 @@ class Admin::ModelsController < Admin::AdminController
 
   before_filter :pre_load
 
-  active_scaffold :model do |config|
-    config.columns = [:name, :manufacturer, :model_groups, :locations, :compatibles]
-    config.columns.each { |c| c.collapsed = true }
-
-  end
-
-
-#################################################################
-
   def index
     unless params[:query].blank?
       @models = Model.find_by_contents("*" + params[:query] + "*", :page => params[:page], :per_page => $per_page)
--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,5 +1,12 @@
 class Admin::RolesController < Admin::AdminController
-  active_scaffold :role
+
+  def index
+    unless params[:query].blank?
+      @roles = Role.find_by_contents("*" + params[:query] + "*", :page => params[:page], :per_page => $per_page)
+    else
+      @roles = Role.paginate :page => params[:page], :per_page => $per_page      
+    end
+  end
   
   
 end
