ff5f3074e32dd75dfbd39f92db88c4e1205f85be
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2008-11-14 14:41:13 +0000

    updating greybox

--- vendor/plugins/greybox/init.rb
+++
@@ -1,9 +0,0 @@
-ActionView::Base.send :include, Greybox
-
-# install files if missing. I prefer this to install.rb because it allows it to be checked out vs installed without issue.
-PUBLIC = File.join([File.dirname(__FILE__), %w(.. .. .. public)].flatten)
-unless File.exists? File.join(PUBLIC, 'greybox')
-  puts "** Installing greybox files in public folder"
-  GREYBOX_ASSETS = File.join(File.dirname(__FILE__), 'assets', 'greybox')
-  FileUtils.cp_r Dir[GREYBOX_ASSETS], PUBLIC
-end
