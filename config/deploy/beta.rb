dfab665c4adb2120ce7215148e5ab600087ced3d
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2009-01-21 18:31:13 +0000

    Beta deployment recipe.

--- config/deploy/beta.rb
+++ config/deploy/beta.rb
@@ -2,6 +2,8 @@ set :application, "leihs2beta"
 set :repository,  "http://code.zhdk.ch/svn/leihs/trunk"
 set :db_config, "/home/rails/leihs/leihs2beta/database.yml"
 set :checkout, :export
+set :use_sudo, false
+
 
 set :rails_env, "production"
 
