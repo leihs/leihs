a64a56b6bee8d2535aa309e987eb0a1e50f1be1b
jerome <jerome@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2009-01-27 16:37:41 +0000

    User in the old leihs model has to be explicitly connected to class_name
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@609 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/models/inventory_import/reservation.rb
+++ app/models/inventory_import/reservation.rb
@@ -1,6 +1,6 @@
 class InventoryImport::Reservation < ActiveRecord::Base
 
-  belongs_to :user
+  belongs_to :user, :class_name => "InventoryImport::User"
   belongs_to :geraetepark
   has_and_belongs_to_many :pakets
   
