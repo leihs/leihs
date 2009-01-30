6b297fc2ce8f2ea24143121b8b83a9813d74ad1d
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2009-01-30 14:30:50 +0000

    Little automatic barcode maker to make all the AVZ barcodes.

---
+++ other/barcode_maker.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/ruby
+
+
+# The EPS output needs Inkscape installed
+
+require 'rubygems'
+require 'barby'
+require 'barby/outputter/png_outputter'
+require 'barby/outputter/cairo_outputter'
+
+class BarcodeMaker
+
+	def initialize(string)
+			@string = string
+      @height = 25
+			@bc = Barby::Code128B.new(@string.to_s)
+	end
+
+	def make_png
+		puts "Making PNG for #{@string}"
+		f = File.open("png/barcode_#{@string}.png", "w")
+	  f.write @bc.to_png(:height => @height, :margin => 5)
+		f.close
+	end
+
+	def make_eps
+		puts "Making EPS for #{@string}"
+		make_svg
+   	infile = "svg/barcode_#{@string}.svg"
+   	outfile = "eps/barcode_#{@string}.eps"
+   	#command = %x[inkscape -z -T -E #\{outfile\} #\{infile\}]
+   	system "inkscape -z -T -E #{outfile} #{infile}"
+	end
+
+	def make_svg
+		unless File.exists?("svg/barcode_#{@string}.svg")
+			puts "Making SVG for #{@string}"
+			f = File.open("svg/barcode_#{@string}.svg", "w") 
+		  f.write @bc.to_svg(:height => @height, :margin => 5)
+			f.close
+		else
+			puts "SVG already there"
+		end
+	end
+
+end
+
+
+
+(1...200000).each do |i|
+	puts "Making barcode #{i.to_s}"
+	foo = BarcodeMaker.new("AVZ#{i.to_s}")
+	foo.make_png
+	foo.make_eps rescue "EPS failed" 
+	foo.make_svg rescue "SVG failed"
+	puts "the world has ended"
+end
+
