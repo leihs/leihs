10ce8a6344995ae1893149c52dd89176547754dd
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2009-02-09 09:47:42 +0000

    New deployment recipe that keeps the attachment images in a shared location.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -26,6 +26,11 @@ task :link_config do
   run "ln -s #{db_config} #{release_path}/config/database.yml"
 end
 
+task	:link_attachments do
+	run "rm -rf #{release_path}/public/images/attachments"
+	run "ln -s #{deploy_to}/#{shared_dir}/attachments #{release_path}/public/images/attachments"
+end
+
 task :remove_htaccess do
 	# Kill the .htaccess file as we are using mongrel, so this file
 	# will only confuse the web server if parsed.
@@ -50,5 +55,6 @@ namespace :deploy do
 end
 
 after "deploy:symlink", :link_config
+after "deploy:symlink", :link_attachments
 before "deploy:restart", :remove_htaccess
 before "deploy:restart", :make_tmp
