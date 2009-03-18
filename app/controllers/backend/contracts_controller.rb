1125039e77c85522f18845d5d6d6ba31bc0e121e
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2009-03-18 13:40:35 +0000

    Rudimentary value list PDF support.

--- app/controllers/backend/contracts_controller.rb
+++ app/controllers/backend/contracts_controller.rb
@@ -18,8 +18,13 @@ class Backend::ContractsController < Backend::BackendController
   
   def show
     respond_to do |format|
+			# Evil hack? We need the contract information in that other template as well
+			if params[:template] == "value_list"
+      format.pdf { send_data(render(:template => 'backend/contracts/value_list', :layout => false), :filename => "value_list_#{@contract.id}.pdf") }
+			else
       # format.html
       format.pdf { send_data(render(:layout => false), :filename => "contract_#{@contract.id}.pdf") }
+			end
     end
   end
 
