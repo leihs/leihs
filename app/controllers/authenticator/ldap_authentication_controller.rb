d61d6b83499ac1ae412608da69b1ea0ab93ef914
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-01-20 15:17:13 +0000

    Porting LDAP fix from 2.0 branch.

--- app/controllers/authenticator/ldap_authentication_controller.rb
+++ app/controllers/authenticator/ldap_authentication_controller.rb
@@ -17,33 +17,55 @@ class Authenticator::LdapAuthenticationController < Authenticator::Authenticator
       if user == "" || password == ""
         flash[:notice] = _("Empty Username and/or Password")
       else
+        bind_dn = LDAP_CONFIG[RAILS_ENV]["bind_dn"]
+        bind_pwd = LDAP_CONFIG[RAILS_ENV]["bind_pwd"]
         ldap = Net::LDAP.new :host => LDAP_CONFIG[RAILS_ENV]["host"],
                           :port => LDAP_CONFIG[RAILS_ENV]["port"].to_i,
                           :encryption => LDAP_CONFIG[RAILS_ENV]["encryption"].to_sym,
                           :base => LDAP_CONFIG[RAILS_ENV]["base"],
-                          :auth=>{:method=>:simple, :username => user, :password => password } 
+                          :auth=>{:method=>:simple, :username => bind_dn, :password => bind_pwd } 
       
-        begin      
+        begin
           if ldap.bind
-            users = ldap.search(:base => LDAP_CONFIG[RAILS_ENV]["base"], :filter => Net::LDAP::Filter.eq(LDAP_CONFIG[RAILS_ENV]["search_field"], "*#{user}"))
-            if users.size == 1            
-              u = User.find_by_login(user)
-              if not u
-                u = User.create(:login => user, :email => users.first.mail)
-                role = Role.find_by_name("customer")
-                InventoryPool.all.each do |ip|
-                  u.access_rights.create(:inventory_pool_id => ip, :role => role, :level => 1)
+            users = ldap.search(:base => LDAP_CONFIG[RAILS_ENV]["base"], :filter => Net::LDAP::Filter.eq(LDAP_CONFIG[RAILS_ENV]["search_field"], "#{user}"))
+
+            if users.size == 1
+              email = users.first.mail if users.first.mail
+              email ||= "#{user}@hkb.bfh.ch"
+              bind_dn = users.first.dn
+              ldap = Net::LDAP.new :host => LDAP_CONFIG[RAILS_ENV]["host"],
+                          :port => LDAP_CONFIG[RAILS_ENV]["port"].to_i,
+                          :encryption => LDAP_CONFIG[RAILS_ENV]["encryption"].to_sym,
+                          :base => LDAP_CONFIG[RAILS_ENV]["base"],
+                          :auth=>{:method=>:simple, :username => bind_dn, :password => password } 
+              if ldap.bind
+             
+                u = User.find_by_login(user)
+               
+                if not u
+                  u = User.create(:login => user, :email => "#{email}")
+                  role = Role.find_by_name("customer")
+                  InventoryPool.all.each do |ip|
+                    u.access_rights.create(:inventory_pool_id => ip, :role => role, :level => 1)
+                  end
                 end
+                u.firstname = users.first["givenname"].to_s 
+                u.lastname = users.first["sn"].to_s
+                u.phone = users.first["telephonenumber"].to_s
+
+                u.save
+                self.current_user = u
+                redirect_back_or_default("/")
+                return true
+              else
+                flash[:notice] = _("Wrong Username/Password")
               end
-              self.current_user = u
-              redirect_back_or_default("/")
-              return true
             else
               flash[:notice] = _("User unknown") if users.size == 0
               flash[:notice] = _("Too many users found") if users.size > 0
             end
           else
-            flash[:notice] = _("Invalid username/password")
+            flash[:notice] = _("Invalid technical user - contact your leihs admin")
             redirect_to :action => 'login'
           end
         rescue Net::LDAP::LdapError
