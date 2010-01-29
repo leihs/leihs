e725846d5919d63860085a53db3ee475f7b78ed4
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-01-29 16:07:04 +0000

    temporary solution for date_select month names

--- config/initializers/gettext.rb
+++ config/initializers/gettext.rb
@@ -13,3 +13,19 @@
 #  GetText.locale = "de_CH"
 #  set_locale "de_CH"
 #  bindtextdomain('leihs')
+
+
+# TODO 2901 make I18n and GetText working together  
+module ActionView
+  module Helpers
+    module DateHelper
+      def date_select(object_name, method, options = {}, html_options = {})
+        #sellittf#
+        default_options = { :use_month_names => Date::MONTHNAMES }
+        options = default_options.merge(options)
+        
+        InstanceTag.new(object_name, method, self, options.delete(:object)).to_date_select_tag(options, html_options)
+      end
+    end
+  end
+end
