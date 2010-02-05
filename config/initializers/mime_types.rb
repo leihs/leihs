61cbfa87d7503c2460006290c805dc58b57b60d5
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-02-05 14:15:00 +0000

    avoid double definition of pdf mime_type
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@1425 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,6 +4,6 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::Type.register "application/pdf", :pdf
+#Mime::Type.register "application/pdf", :pdf
 Mime::Type.register_alias "text/html", :auto_complete
 
