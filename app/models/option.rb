141eb585296c08a146ef69cce3f4380b5d4cf153
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-03-04 15:17:24 +0000

    validation options and cleaning up the database through migration
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@1493 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/models/option.rb
+++ app/models/option.rb
@@ -3,6 +3,11 @@ class Option < ActiveRecord::Base
   has_many :option_lines
 
   validates_presence_of :inventory_pool, :name
+  validates_uniqueness_of :inventory_code, :scope => :inventory_pool_id, :unless => Proc.new { |record| record.inventory_code.blank? }
+
+  before_validation do |record|
+    record.inventory_code = nil if !record.inventory_code.nil? and record.inventory_code.blank? 
+  end
 
   define_index do
     indexes :inventory_code
