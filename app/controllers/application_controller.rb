8d6caa7351419c4dd875a82f2e80579c68db909c
tpo <tpo@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-03-04 15:07:10 +0000

    move quick profiling into its own helper
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@1492 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -71,43 +71,4 @@ class ApplicationController < ActionController::Base
     super
   end
 
-  helper_method :t, :t_init, :t_tresh
-
-  # Output profiling/timing information
-  #
-  #   <%
-  #      t_init       # this is needed for initialization
-  #      t_tresh 0.03 # log treshold
-  #    %>
-  #      ...
-  #   <% t "1"; do some stuff %>
-  #   <% t "2"; do different stuff %>
-  #
-  # will output time elapsed between step "1" and step "2" if it was longer
-  # than 0.03s.
-  #
-  # The "t" method can also be used inside controllers etc.
-  #
-  def t(step_id)
-    now = Time.now
-    diff = now - @t_last
-    if diff > @t_tresh
-      puts "#{step_id} #{diff}"
-    end
-    @t_last = now
-  end
-
-  # see #t
-  #
-  def t_init
-    @t_last = Time.now
-  end
-
-  # see #t
-  #
-  def t_tresh( treshold )
-    @t_tresh = treshold
-  end
-
-
 end
