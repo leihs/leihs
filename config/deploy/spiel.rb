21880810d7c95361addcaf5d39bc171b57d6fa14
rca <rca@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-04-14 09:34:16 +0000

    Might as well activate this bit of the deploy recipes.
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@1607 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- config/deploy/spiel.rb
+++ config/deploy/spiel.rb
@@ -73,7 +73,7 @@ task :configure_sphinx do
 end
 
 task :stop_sphinx do
-#  run "cd #{previous_release} && RAILS_ENV='production' rake ts:stop"
+  run "cd #{previous_release} && RAILS_ENV='production' rake ts:stop"
 end
 
 task :start_sphinx do
