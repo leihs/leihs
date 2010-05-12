fccab019f26bf563fcdc6ebe83f52f61bc6a7b52
tpo <tpo@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-05-12 10:46:14 +0000

    Feature: don't show models that have only packaged items
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@1658 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

---
+++ features/step_definitions/package_steps.rb
@@ -0,0 +1,28 @@
+Given "a package '$package' exists" do |package|
+  Given "a model '#{package}' exists"
+  model = Model.find_by_name package
+  model.is_package = true
+  model.save
+end
+
+Given "item $item is part of package item $package_item" do |item, package_item|
+  package = Item.find_by_inventory_code package_item
+  item    = Item.find_by_inventory_code item
+  item.parent = package
+  item.save
+end
+
+When /^lending_manager clicks to add an additional model$/ do
+  get_via_redirect add_line_backend_inventory_pool_user_acknowledge_path(@inventory_pool, @order.user, :start_date => Date.today, :end_date => Date.today)
+  #follow_redirect!
+end
+
+#Then /^he sees 0 lines 'Khil Remix'$/ do
+# the check actually doesn't bother about lines at all...
+Then /^(.*) sees (\d*) line(s?) '(.*)'$/ do |who, number, plural, text|
+  @response.body.scan(text).count.should == number.to_i
+end
+
+Then "even though 'Khil Remix' is not part of a package in inventory pool 2!" do
+  # dummy - has comment only!
+end
