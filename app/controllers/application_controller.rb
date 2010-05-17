031ca5e0c21797a1f1a09ec5a937952cd8775ce2
sellittf <sellittf@1fac63f3-6ac2-4d65-bf7b-275fb988ef2c>
2010-05-17 08:59:18 +0000

    prevent password logging
    git-svn-id: http://code.zhdk.ch/svn/leihs/trunk@1660 1fac63f3-6ac2-4d65-bf7b-275fb988ef2c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,9 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'a51355e168a2870e8e42d11f9390b986'
+
+  # Scrub sensitive parameters from your log
+  filter_parameter_logging :password
   
   $theme = '00-patterns'
   $layout_public_path = '/layouts/' + $theme
