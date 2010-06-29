84ec6185dbbb9f96fde14c53bdd8c890799b264f
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-06-29 11:39:11 +0200

    Move to Culerity which does htmlunit view testing

---
+++ config/environments/culerity.rb
@@ -0,0 +1,23 @@
+#config.cache_classes = false
+config.cache_classes = true # tpospise -> false breaks cucumber #
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
+config.action_view.cache_template_loading            = false
+
+# Disable request forgery protection in test environment
+config.action_controller.allow_forgery_protection    = false
+
+# Tell Action Mailer not to deliver emails to the real world.
+# The :test delivery method accumulates sent emails in the
+# ActionMailer::Base.deliveries array.
+config.action_mailer.delivery_method = :persistent
+
+config.after_initialize do
+  require 'culerity/persistent_delivery'
+end
+
