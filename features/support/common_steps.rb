f32095348d71d75448159e78a2a4218560452631
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-06-30 11:09:54 +0200

    allow other pending'' explanations

--- features/support/common_steps.rb
+++ features/support/common_steps.rb
@@ -6,6 +6,10 @@ Given "pending - reported by $who on $date" do |who, date|
   pending
 end
 
+Given /pending - (?!reported by)(.*)/ do |explanation|
+  pending
+end
+
 When /we wait (\d+) second(s?)/ do |seconds, plural|
   sleep seconds.to_i
 end
