a96422ecb251c527a9ffdfa22b2e8cc0608f7dc7
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-07-12 12:54:24 +0200

    Tests: Allow switching of JS for a reason

--- features/support/common_steps.rb
+++ features/support/common_steps.rb
@@ -14,4 +14,7 @@ When /(\w+) wait (\d+) second(s?)/ do |who, seconds, plural|
   sleep seconds.to_i
 end
 
-
+When "I switch off JavaScript because $reason" do |reason|
+  # see https://sourceforge.net/tracker/index.php?func=detail&aid=2969230&group_id=47038&atid=448266
+  $browser.javascript_enabled=false
+end
