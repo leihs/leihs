0da9be6b89493cdaafe7248acfeaaa456e46ef1f
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-07-12 17:29:46 +0200

    differ between test and implementation pending

--- features/support/common_steps.rb
+++ features/support/common_steps.rb
@@ -6,6 +6,14 @@ Given "pending - reported by $who on $date" do |who, date|
   pending
 end
 
+Given "pending - resolved by $who on $date" do |who, date|
+  pending
+end
+
+Given /^reported by (.*) on (.*)$/ do |who, date|
+  # do nothing
+end
+
 Given /pending - (?!reported by)(.*)/ do |explanation|
   pending
 end
