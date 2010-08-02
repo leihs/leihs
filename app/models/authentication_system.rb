e41de5551fc74dc07eee5bc1d10082a7adc0d038
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-08-02 13:35:57 +0200

    Requiring address information from users who have ZHdK logins.

--- app/models/authentication_system.rb
+++ app/models/authentication_system.rb
@@ -7,9 +7,12 @@ class AuthenticationSystem < ActiveRecord::Base
   def missing_required_fields(user)
     case self.class_name
       when "Zhdk"
-        required_fields = [:email, :phone]
+        required_fields = [:email, :phone, :address, :zip, :city]
         required_fields.delete(:email) if !user.email.blank? and !user.extended_info["email_alt"].blank?
         required_fields.delete(:phone) if !user.phone.blank? or !user.extended_info["phone_mobile"].blank?
+        required_fields.delete(:address) if !user.address.blank?
+        required_fields.delete(:zip) if !user.zip.blank?
+        required_fields.delete(:city) if !user.city.blank?
         required_fields
       else
         []
