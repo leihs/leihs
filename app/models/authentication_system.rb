656a2d39307b6c1b1aed0352b69d80154f73e3c7
Franco Sellitto <franco.sellitto@zhdk.ch>
2010-08-03 12:36:53 +0200

    [BUGFIX] fetch properly user.extended_info, doesn't explode if nil

--- app/models/authentication_system.rb
+++ app/models/authentication_system.rb
@@ -8,8 +8,8 @@ class AuthenticationSystem < ActiveRecord::Base
     case self.class_name
       when "Zhdk"
         required_fields = [:email, :phone, :address, :zip, :city]
-        required_fields.delete(:email) if !user.email.blank? and !user.extended_info["email_alt"].blank?
-        required_fields.delete(:phone) if !user.phone.blank? or !user.extended_info["phone_mobile"].blank?
+        required_fields.delete(:email) if !user.email.blank? and !user.extended_info.try(:fetch, "email_alt").blank?
+        required_fields.delete(:phone) if !user.phone.blank? or !user.extended_info.try(:fetch, "phone_mobile").blank?
         required_fields.delete(:address) unless user.address.blank?
         required_fields.delete(:zip) unless user.zip.blank?
         required_fields.delete(:city) unless user.city.blank?
