1f4d5d5c299d31c73857a6f289a12fc4d9688223
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2010-08-03 10:47:54 +0200

    More reasonable file size limit

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,7 +2,7 @@ class Attachment < ActiveRecord::Base
 
     belongs_to :model
     
-    has_attachment :size => 1.kilobytes..20.megabytes,
+    has_attachment :size => 1.kilobytes..100.megabytes,
                    :storage => :file_system, :path_prefix => 'public/attachments'
 
     validates_as_attachment
