22c5671da9dbb2add2583e11522f410ef72ab04a
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-08-31 18:22:21 +0200

    remove stale old commented out code

--- app/models/available_quantity.rb
+++ app/models/available_quantity.rb
@@ -2,10 +2,8 @@ class AvailableQuantity < ActiveRecord::Base
   belongs_to :availability_change
   belongs_to :group
 
-#old#  validates_presence_of :status_const
   validates_presence_of :availability_change_id
   validates_presence_of :group_id
-#old#  validates_presence_of :quantity
   validates_presence_of :in_quantity
   validates_presence_of :out_quantity
 
