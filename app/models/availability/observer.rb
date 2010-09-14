dac4fe759b1b6eb6b283abf153efc58b0b4dd77a
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-09-14 10:21:55 +0200

    observer need to react to Item_Lines and not generally to Contract_Lines

--- app/models/availability/observer.rb
+++ app/models/availability/observer.rb
@@ -1,15 +1,17 @@
 module Availability
 
     class Observer < ActiveRecord::Observer
-      observe :order_line, :contract_line, :item
+      observe :order_line, :item_line, :item
       
       #tmp#3 TODO bulk recompute if many lines are updated together
       
       def recompute(record)
+        debugger
         if record.is_a?(Item) and record.inventory_pool
             Availability::Change.recompute(record.model, record.inventory_pool)
-        elsif (record.is_a?(OrderLine) and record.order.status_const == Order::SUBMITTED) or record.is_a?(ContractLine)
-            Availability::Change.recompute(record.model, record.document.inventory_pool)
+        elsif (record.is_a?(OrderLine) and record.order.status_const == Order::SUBMITTED) or record.is_a?(ItemLine)
+          puts record.inspect, "--------------"
+            Availability::Change.recompute(record.model, record.document.inventory_pool) unless record.model_id.nil?
         end
       end
       
