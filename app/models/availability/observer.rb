8fed26736a5214c61a586b0878e7901004e7c29f
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-09-14 10:24:42 +0200

    remove debug noise etc.

--- app/models/availability/observer.rb
+++ app/models/availability/observer.rb
@@ -6,12 +6,10 @@ module Availability
       #tmp#3 TODO bulk recompute if many lines are updated together
       
       def recompute(record)
-        debugger
         if record.is_a?(Item) and record.inventory_pool
             Availability::Change.recompute(record.model, record.inventory_pool)
         elsif (record.is_a?(OrderLine) and record.order.status_const == Order::SUBMITTED) or record.is_a?(ItemLine)
-          puts record.inspect, "--------------"
-            Availability::Change.recompute(record.model, record.document.inventory_pool) unless record.model_id.nil?
+            Availability::Change.recompute(record.model, record.document.inventory_pool)
         end
       end
       
