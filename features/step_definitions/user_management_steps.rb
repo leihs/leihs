092c694a8c4ad2ac651020946e44a28378ec187b
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-11-09 09:52:29 +0100

    whitespace: make 80 column friendly

--- features/step_definitions/user_management_steps.rb
+++ features/step_definitions/user_management_steps.rb
@@ -7,15 +7,15 @@ end
 
 Given "user '$who' has access to inventory pool $ip_s" do |who, ip_s|
   inventory_pools = ip_s.split(" and ").collect { | ip_name |
-	                                          InventoryPool.find_by_name ip_name
-                                                }
+    InventoryPool.find_by_name ip_name
+  }
   user = Factory.create_user({:login => who
-                                #, :password => "pass"
-                              },
-			      { :inventory_pool => inventory_pools.first })
-  inventory_pools.each do |ip|
+                          #, :password => "pass"
+                             },
+			     { :inventory_pool => inventory_pools.first })
+  inventory_pools.each { |ip|
     Factory.define_role(user, ip, "customer" )
     user.inventory_pools.include?(ip).should == true
-  end
+  }
 end
 
