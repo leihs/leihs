e4301b7c5627615019e2dbfa85095bc20ce62641
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-11-09 09:49:47 +0100

    drop stale customer level step

--- features/step_definitions/user_management_steps.rb
+++ features/step_definitions/user_management_steps.rb
@@ -5,16 +5,6 @@ Given /^a customer "([^"]*)"$/ do |name|
   r.save
 end
 
-# TODO: drop this level stuff
-Given "User '$name' is a '$level' customer" do |name, level|
-  @user = Factory.create_user({:login => name
-                                #, :password => "pass"
-                              }, {:role => 'customer'})
-  r = @user.access_rights.first
-#tmp#9  r.level = AccessRight::LEVELS[level]
-  r.save
-end
-
 Given "user '$who' has access to inventory pool $ip_s" do |who, ip_s|
   inventory_pools = ip_s.split(" and ").collect { | ip_name |
 	                                          InventoryPool.find_by_name ip_name
