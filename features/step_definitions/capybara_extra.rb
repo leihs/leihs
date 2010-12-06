5cf101af682e00a94829441f300a90432e76bdc9
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-12-06 16:55:38 +0100

    Add capybara debugging steps

---
+++ features/step_definitions/capybara_extra.rb
@@ -0,0 +1,9 @@
+When /^I dump the response$/ do
+  puts body
+end
+
+When /^I dump the response to '([^']*)'$/ do |filename|
+  File.open(filename, File::CREAT|File::TRUNC|File::RDWR) do |f|
+    f.puts body
+  end
+end
