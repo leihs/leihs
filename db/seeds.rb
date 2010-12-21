b784e6e39a7fce9cec07111399e667b49e2e02df
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2010-12-21 11:13:45 +0100

    Re-introduce: report the superuser being created
    when running db:seed
    (forgot to check in this file)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,6 @@ require( RAILS_ROOT + '/lib/factory.rb')
 Factory.create_default_languages
 Factory.create_default_authentication_systems
 Factory.create_default_roles
-Factory.create_super_user
+superuser = Factory.create_super_user
+puts _("The administrator %{a} has been created ") % { :a => superuser.login }
 Factory.create_default_building
