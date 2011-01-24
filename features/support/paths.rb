0cdaf187907568412ce11b601ae409d734c0b35e
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-01-24 15:16:07 +0100

    Add 'logout' path, so we don't have to go through JS to log out
    Makes tests faster without sacrificing much stack coverage

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,6 +11,9 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
 
+    when /logout/
+      '/logout'
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
