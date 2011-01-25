9ca777a68f5ea4a80256e3dfa21a5e9f1851dea2
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-01-25 16:55:04 +0100

    Alow changing the defaul delay

--- features/step_definitions/debug_steps.rb
+++ features/step_definitions/debug_steps.rb
@@ -26,26 +26,38 @@ end
 # Tools to slow down execution, to help following and debugging
 # tests
 
+# When I wait
+#
+When /^(\w+) wait(s?)$/ do |who, plural|
+  @delay ||= 2 # default
+  When "#{who} wait#{plural} #{@delay} seconds"
+end
+
 When /(\w+) wait (\d+) second(s?)/ do |who, seconds, plural|
   sleep seconds.to_i
 end
 
 # tag your scenarion with '@slowly' and then every step
-# will be executed with a delay of 2 seconds
+# will be executed with a default delay of 2 seconds
 Before('@slowly') do
-  When "I wait 2 seconds" unless @skip_wait
+  When "I wait" unless @skip_wait
 end
 
 AfterStep('@slowly') do
-  When "I wait 2 seconds" unless @skip_wait
+  When "I wait" unless @skip_wait
 end
 
 # since only Scenarios and not single steps can be tagged with
 # '@slowly', you can switch on and off delaying between steps
-
+#
 # When I switch on waiting
 # When I switch off waiting
+#
 When "I switch $waitstate waiting" do |waitstate|
   @skip_wait = (waitstate == "off")
+end
+
+When "I set the default delay to $delay" do |delay|
+  @delay = delay.to_i
 end
 
