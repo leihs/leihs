800b227dda5b778c7e7da3dbb16a925a2b5993b1
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-01-31 16:26:42 +0100

    Stay in greybox when sending mail
    Fixes: https://www.pivotaltracker.com/story/show/7188311
    
    All test run through

--- app/helpers/backend/mails_helper.rb
+++ app/helpers/backend/mails_helper.rb
@@ -1,14 +1,15 @@
 module Backend::MailsHelper
-  def mail_link( current_inventory_pool, user, source_path, options = nil)
+  def mail_link( current_inventory_pool, user, source_path, options = {})
+    modal = options.slice(:layout)
     newmailpath = \
       if current_inventory_pool
-        new_backend_inventory_pool_mail_path(:inventory_pool => current_inventory_pool,
-                                             :user_id => user.try(:id),
-                                             :source_path => source_path)
+        new_backend_inventory_pool_mail_path( { :inventory_pool => current_inventory_pool,
+                                                :user_id => user.try(:id),
+                                                :source_path => source_path}.merge(modal) )
       else
-        new_backend_mail_path(:inventory_pool_id => current_inventory_pool.try(:id),
-                              :user_id => user.try(:id),
-                              :source_path => source_path)
+        new_backend_mail_path( { :inventory_pool_id => current_inventory_pool.try(:id),
+                                 :user_id => user.try(:id),
+                                 :source_path => source_path }.merge(modal))
       end
 
     link_to icon_tag("email_edit") + _("Write Email"), newmailpath, options
