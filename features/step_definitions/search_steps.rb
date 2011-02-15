b482bc623f8476a582f93e98c3358559fc455c58
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-02-15 14:04:48 +0100

    Ensure coverage with partials of possible search results

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -2,3 +2,12 @@ When 'I sort by "$sort_key"' do |sort_key|
   When "I click \"#{sort_key}\""
 end
 
+When "I count the number of indexed models" do
+  # the following test will only work under Unix...
+  @number = `grep -l define_index #{Rails.root}/app/models/* | wc -l`
+end
+
+Then "that number must be the same as the number of search partials" do
+  # see app/views/backend/backend/search/_nilclass.html.erb on why we exclude nilclass
+  @number.should == `ls #{Rails.root}/app/views/backend/backend/search | grep -v nilclass | wc -l`
+end
