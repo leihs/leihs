0acdf190b580bfeb7babaf7b72fd7cc87f1ffd3f
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-02-28 15:45:14 +0100

    Refactor timeline
    split into methods to make code easier to understand

--- app/helpers/backend/models_helper.rb
+++ app/helpers/backend/models_helper.rb
@@ -4,74 +4,24 @@ module Backend::ModelsHelper
   # availability and allocation of a model over time
   #
   def timeline(model, inventory_pool)
-    content_for :head do
-      r = javascript_tag do
-        <<-HERECODE
-          Timeline_ajax_url='/javascripts/simile_timeline/timeline_ajax/simile-ajax-api.js';
-          Timeline_urlPrefix='/javascripts/simile_timeline/timeline_js/';
-          Timeline_parameters='bundle=true&forceLocale=#{locale.language}';
-        HERECODE
-      end
-      r += javascript_include_tag "simile_timeline/timeline_js/timeline-api.js"
-      r += javascript_tag do
-        "SimileAjax.History.enabled = false;"
-      end
-      r += stylesheet_link_tag 'timeline'
-    end
+    set_timeline_headers()
 
     events = {}
     partition = model.partitions.in(inventory_pool).current_partition
     changes = model.availability_changes_in(inventory_pool).changes
 
-    model.running_reservations(inventory_pool).each do |line|
-      color = if not line.item
-                '#90c1c8'
-              elsif line.is_late?
-                'red'
-              elsif line.returned_date
-                '#e1e157'
-              else
-                '#e3aa01'
-              end
-      group_id = line.allocated_group.try(:id).to_i
+    events = reservations_to_events( model.running_reservations(inventory_pool))
+   
 
-      extra_info = line.item.try(:inventory_code) || _("Quantity: %d") % line.quantity
-      title = "#{line.document.user} (#{extra_info})"
-
-      link_string, link_path =
-        if line.is_a?(OrderLine)
-          [ icon_tag("accept") + _("Acknowledge"),
-            backend_inventory_pool_user_acknowledge_path(current_inventory_pool, line.document.user, line.document) ]
-        elsif line.document.status_const == Contract::UNSIGNED
-          [ icon_tag("arrow_turn_right") + _("Hand Over"),
-            backend_inventory_pool_user_hand_over_path(current_inventory_pool, line.document.user) ]
-        else
-          [ icon_tag("arrow_undo") + _("Take Back"),
-            backend_inventory_pool_user_take_back_path(current_inventory_pool, line.document.user) ]
-        end
- 
-      document_link = content_tag :div, :class => "buttons", :style => "margin: 1.5em;" do
-                        link_to link_string, link_path
-                      end
-      description = "Group: #{line.allocated_group}<br />Phone: #{line.document.user.phone}<br />#{document_link}"
-      events[group_id] ||= []
-      events[group_id] << {:start => line.start_date.to_time.to_s(:rfc822),
-                           :end => (line.end_date.tomorrow.to_time - 1.second).to_s(:rfc822),
-                           :durationEvent => true,
-                           :title => title,
-                           :description => description,
-                           #:trackNum => (events[group_id].empty? ? 0 : (line.item ? events[group_id].collect {|e| e[:trackNum] }.compact.max.to_i.next : nil)),
-                           :color => color,
-                           :textColor => 'black',
-                           :classname => (!line.item and !line.available? ? "unavailable" : nil) }
-    end # model.running_reser...
-
+    # iterate through events and construct JS code that will instantiate
+    # each event
     #eventSource_js = ["eventSource[-1] = new Timeline.DefaultEventSource(); eventSource[-1].loadJSON(#{{:events => events.values.flatten}.to_json}, document.location.href);"]
     eventSource_js = []
     events.each_pair do |group_id, event|
       json = {:events => event}.to_json 
       eventSource_js << "eventSource[#{group_id}] = new Timeline.DefaultEventSource(); eventSource[#{group_id}].loadJSON(#{json}, document.location.href);"
     end
+
 
     # TODO dynamic timeZone, get rid of GMT in the bubble
     sum_w = 35
@@ -103,6 +53,7 @@ module Backend::ModelsHelper
         (d.empty? ? nil : d.join(', '))
       end.compact
     end
+
 
     # TODO automatic autowidth
     r = javascript_tag do
@@ -159,5 +110,79 @@ module Backend::ModelsHelper
     
     r += content_tag :div, :id => "my_timeline", :style => "height: #{sum_w}px; border: 1px solid #aaa" do end
   end
-  
+
+  def set_timeline_headers()
+    content_for :head do
+      r = javascript_tag do
+        <<-HERECODE
+          Timeline_ajax_url='/javascripts/simile_timeline/timeline_ajax/simile-ajax-api.js';
+          Timeline_urlPrefix='/javascripts/simile_timeline/timeline_js/';
+          Timeline_parameters='bundle=true&forceLocale=#{locale.language}';
+        HERECODE
+      end
+      r += javascript_include_tag "simile_timeline/timeline_js/timeline-api.js"
+      r += javascript_tag do
+        "SimileAjax.History.enabled = false;"
+      end
+      r += stylesheet_link_tag 'timeline'
+    end
+  end
+
+
+  # iterate through model reservations and prepare necessary parameters
+  # to construct events inside with Timeline
+  def reservations_to_events( running_reservations )
+    events = {}
+    running_reservations.each do |line|
+      color = if not line.item
+                '#90c1c8'
+              elsif line.is_late?
+                'red'
+              elsif line.returned_date
+                '#e1e157'
+              else
+                '#e3aa01'
+              end
+      group_id = line.allocated_group.try(:id).to_i
+
+      events[group_id] ||= []
+      events[group_id] << {:start         => line.start_date.to_time.to_s(:rfc822),
+                           :end           => (line.end_date.tomorrow.to_time - 1.second).to_s(:rfc822),
+                           :durationEvent => true,
+                           :title         => event_title(line),
+                           :description   => event_description(line),
+                           #:trackNum     => (events[group_id].empty? ? 0 : (line.item ? events[group_id].collect {|e| e[:trackNum] }.compact.max.to_i.next : nil)),
+                           :color         => color,
+                           :textColor     => 'black',
+                           :classname     => (!line.item and !line.available? ? "unavailable" : nil) }
+    end
+    events
+  end
+
+  def event_title(line)
+      extra_info = line.item.try(:inventory_code) || _("Quantity: %d") % line.quantity
+      return "#{line.document.user} (#{extra_info})"
+  end
+
+  # construct the popup that is being displayed when you click on
+  # an event
+  def event_description(line)
+
+      link_string, link_path =
+        if line.is_a?(OrderLine)
+          [ icon_tag("accept") + _("Acknowledge"),
+            backend_inventory_pool_user_acknowledge_path(current_inventory_pool, line.document.user, line.document) ]
+        elsif line.document.status_const == Contract::UNSIGNED
+          [ icon_tag("arrow_turn_right") + _("Hand Over"),
+            backend_inventory_pool_user_hand_over_path(current_inventory_pool, line.document.user) ]
+        else
+          [ icon_tag("arrow_undo") + _("Take Back"),
+            backend_inventory_pool_user_take_back_path(current_inventory_pool, line.document.user) ]
+        end
+ 
+      document_link = content_tag :div, :class => "buttons", :style => "margin: 1.5em;" do
+                        link_to link_string, link_path
+                      end
+      return "Group: #{line.allocated_group}<br />Phone: #{line.document.user.phone}<br />#{document_link}"
+  end
 end
