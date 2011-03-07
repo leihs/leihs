79824b5dcd9180d4557e304fdb4c7b02c38016b4
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-03-07 13:39:36 +0100

    Explain what Event objects are

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,6 +9,24 @@
 #  contract_line_ids :string
 #
 
+# This represents a date associated with a customer on which something
+# particular happens with customer's contracts.
+#
+# It can be the fact that the customer should come to pick up items
+# because they are reserved from that date on.
+#
+# Another Event could represent the date on which the customer should
+# return Items because they only were reserved up to that date.
+#
+# Etc.
+#
+# The Event object does not contain a reference to what exactly it is
+# that would happen on that date. As such it is up to the user of the
+# event to give it a meaning.
+#
+# Events do not have a corresponding database table associated with them
+# a accordingly are not persistent!
+#
 # TODO drop completely this class!!!
 class Event < ActiveRecord::Base
   def self.columns() @columns ||= []; end
