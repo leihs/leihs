dcf65a9fe70ac70645ae1fc03a1a5bb46e31b399
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-04-19 16:57:37 +0200

    Move availability_changes helper into haml partial

--- app/helpers/backend/availability_helper.rb
+++ app/helpers/backend/availability_helper.rb
@@ -7,57 +7,6 @@ module Backend::AvailabilityHelper
     end if g    
   end
 
-  # Display a table with the changes of availability of a model
-  # along with information on who the customer is that borrowed
-  # the item on that day.
-  #
-  def availability_changes(availability)
-    groups = [Group::GENERAL_GROUP_ID] + availability.inventory_pool.groups
-    content_tag :table do
-      availability.changes.collect do |c|
-        a = content_tag :tr do
-          [_("Borrowable %s") % short_date(c.date),
-           _("In Stock (%d)") % c.quantities.collect(&:in_quantity).sum,
-           _("Not In Stock (%d)") % c.quantities.collect(&:out_quantity).sum,
-           _("DocumentLines")].collect do |s|
-            content_tag :th do
-              s  
-            end
-          end.join
-        end
-        
-        a += groups.collect do |group|
-          aq = c.quantities.detect {|q| q.group_id == group.try(:id) }
-          in_quantity = aq.try(:in_quantity).to_i
-          out_quantity = aq.try(:out_quantity).to_i
-          next if in_quantity.zero? and out_quantity.zero? 
-          content_tag :tr do
-            b = content_tag :td do
-              "#{(group ? group : _("General"))}:"
-            end
-            b += [in_quantity, out_quantity].collect do |q|
-              content_tag :td, :class => (q < 0 ? "valid_false" : nil) do
-                q
-              end
-            end.join
-            b += content_tag :td do
-              content_tag :ol do
-                aq.document_lines.collect do |dl|
-                  content_tag :li do
-                    extra_info = dl.item.try(:inventory_code) || _("Qty: %d") % dl.quantity
-                    link_to \
-                      "#{dl.document.user} (#{extra_info}) => #{short_date(dl.start_date)}-#{short_date(dl.end_date)}",
-                      backend_inventory_pool_user_path(@current_inventory_pool, dl.document.user)
-                  end
-                end.join
-              end
-            end
-          end
-        end.join
-      end.join
-    end
-  end
-
   def availability_periods_merged_groups(model, inventory_pool, user)
     start_date = Date.today
     end_date = Availability::ETERNITY
