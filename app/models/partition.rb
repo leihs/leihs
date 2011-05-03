087fc8e6956c90556553ae52382ce1906b455cda
Tomáš Pospíšek <tomas.pospisek@zhdk.ch>
2011-05-03 15:12:48 +0200

    Provide some guidance for the needing programmer

--- app/models/partition.rb
+++ app/models/partition.rb
@@ -7,4 +7,6 @@ class Partition < ActiveRecord::Base
   validates_presence_of :model, :inventory_pool, :quantity
   validates_numericality_of :quantity, :only_integer => true, :greater_than => 0
   
+  # also see model.rb->def in(...).
+  # Model extends the "partitions" relation at access time with further methods
 end
