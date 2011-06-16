7de835627db330da5be89ff8005489eb9fd32fc5
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-06-16 19:00:04 +0200

    removing duplicated FastGettext settings

--- config/application.rb
+++ config/application.rb
@@ -48,11 +48,6 @@ module Leihs
   end
 end
 
-#rails3#tmp
-FastGettext.add_text_domain 'leihs', :path => 'locale' #, :type => :po
-FastGettext.default_available_locales = ['en_US','de_CH', 'es', 'fr', 'gsw_CH@bern', 'gsw_CH@zurich', 'gsw_ZH'] #all you want to allow
-FastGettext.default_text_domain = 'leihs'
-
 ########################################################################
 #rails3#
 
