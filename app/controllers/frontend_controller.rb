b736883b8937d16ea6dac1a988cfd34985706c2c
Sebastian Pape <Sebastian@Sebastian-Pape.local>
2011-07-13 11:35:53 +0200

    Franco changed all categorie descendent models thumb images ....

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -11,6 +11,7 @@ class FrontendController < ApplicationController
 
   def index
     @user = current_user
+    @current_categories = (current_user.all_categories & Category.roots).sort
     @missing_fields = @user.authentication_system.missing_required_fields(@user)
     render :template => "users/show", :layout => "frontend_2010" unless @missing_fields.empty?
   end
