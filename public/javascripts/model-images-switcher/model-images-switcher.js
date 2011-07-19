3c1d11c13cfb5b344500b9394b68580a381b37e0
Sebastian Pape <Sebastian@sebastian-pape.home>
2011-07-19 15:28:45 +0200

    (REDESIGN) Model View: Image Switcher and other stuff

--- public/javascripts/model-images-switcher/model-images-switcher.js
+++ public/javascripts/model-images-switcher/model-images-switcher.js
@@ -5,49 +5,47 @@
  *
  * @name Model Images Switcher
  * @author Sebastian Pape <email@sebastianpape.com>
- * @last-change 15.07 14:39
 */
 
 $(document).ready(function(){
-    setupSameDimensions();
-	setupBindings();
+	ImageSwitcher.setupBindings();
 });
 
-function setupSameDimensions() {
-    $('#model #bigimages .hidden').each(function(){
-        $(this).attr("height", $('#model #bigimages .first').height());
-    });
-}
+var ImageSwitcher = new ImageSwitcher();
 
-function setupBindings() {
-    // MOUSEENTER
-    $('#model #smallimages img').bind('mouseenter', function(){
-        hoveredItem = $(this);
-        $('#model #bigimages img').each(function() {
-            if($(this).attr('src') == hoveredItem.attr('src').replace(/_thumb/g, "")) {
-                $(this).stop().fadeIn();
-                $('#model #bigimages .first').stop().animate({
-                    opacity: 0
-                });
-            }
-        });
-    });
+function ImageSwitcher() {
     
-    // MOUSELEAVE
-    $('#model #smallimages img').bind('mouseleave', function(){
-        hoveredItem = $(this);
-        $('#model #bigimages img').each(function() {
-            if($(this).attr('src') == hoveredItem.attr('src').replace(/_thumb/g, "")) {
-                if($(this).css("opacity") != 1) {
-                    $(this).removeAttr("style");
+    this.setupBindings = function() {
+        // MOUSEENTER
+        $('#model #smallimages img').bind('mouseenter', function(){
+            hoveredItem = $(this);
+            $('#model #hiddenimages img').each(function() {
+                $(this).stop().hide();
+                if($(this).attr('src') == hoveredItem.attr('src').replace(/_thumb/g, "")) {
+                    $(this).stop().fadeIn();
+                    $('#model #bigimage img').stop().animate({
+                        opacity: 0
+                    });
                 }
-                $(this).stop().fadeOut(400, function(){
-                    $(this).removeAttr("style");
-                });
-                $('#model #bigimages .first').stop().animate({
-                    opacity: 1
-                });
-            }
+            });
         });
-    });
+        
+        // MOUSELEAVE
+        $('#model #smallimages img').bind('mouseleave', function(){
+            hoveredItem = $(this);
+            $('#model #hiddenimages img').each(function() {
+                if($(this).attr('src') == hoveredItem.attr('src').replace(/_thumb/g, "")) {
+                    if($(this).css("opacity") != 1) {
+                        $(this).removeAttr("style");
+                    }
+                    $(this).stop().fadeOut(400, function(){
+                        $(this).removeAttr("style");
+                    });
+                    $('#model #bigimage img').stop().animate({
+                        opacity: 1
+                    });
+                }
+            });
+        });
+    }
 }
