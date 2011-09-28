7876335eaaf400cfb2129b87eed31b6dada7ec19
Sebastian Pape <Sebastian@SPape.local>
2011-09-28 17:27:47 +0200

    modify availability_periods_for_user (Model)

--- app/models/availability/model.rb
+++ app/models/availability/model.rb
@@ -32,13 +32,19 @@ module Availability
       end.sum
     end
 
-    def availability_periods_for_user(user, start_date = Date.today, end_date = Availability::ETERNITY)
+    def availability_periods_for_user(user, with_total_borrowable = false, start_date = Date.today, end_date = Availability::ETERNITY)
       (inventory_pools & user.inventory_pools).collect do |inventory_pool|
         groups = user.groups.scoped_by_inventory_pool_id(inventory_pool)
-        {:inventory_pool => {:id => inventory_pool.id, :name => inventory_pool.to_s, :address => inventory_pool.address.to_s},
-         #:borrowable => partitions.in(inventory_pool).by_groups(groups.collect(&:id)).sum(:quantity).to_i +
-         #                 partitions.in(inventory_pool).by_group(Group::GENERAL_GROUP_ID),
-         :availability => availability_changes_in(inventory_pool).changes.available_quantities_for_groups(groups, true) }
+        h = {:inventory_pool => {:id => inventory_pool.id,
+                             :name => inventory_pool.to_s,
+                             :address => inventory_pool.address.to_s,
+                             :closed_days => inventory_pool.workday.closed_days },
+             :availability => availability_changes_in(inventory_pool).changes.available_quantities_for_groups(groups, true) }
+        if with_total_borrowable
+          h[:total_borrowable] = partitions.in(inventory_pool).by_groups(groups.collect(&:id)).sum(:quantity).to_i +
+                                 partitions.in(inventory_pool).by_group(Group::GENERAL_GROUP_ID)
+        end
+        h
       end
     end
 
