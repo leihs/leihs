146d5f4f831606e0f753a681beb7d265585edd9a
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-10-17 17:07:48 +0200

    removing old plugins

--- vendor/plugins/model_auto_completer/test/model_auto_completer_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test/unit'
-
-class ModelAutoCompleterTest < Test::Unit::TestCase
-  # By now tests are semi-automated. Download the application (guess the root
-  # from the svn URL), run the migration and check http://localhost:3000.
-end
