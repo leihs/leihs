c53d4dd5592a598785bcaf28627e64268e144154
Ramón Cahenzli <rca@psy-q.ch>
2011-10-20 17:48:49 +0200

    More awesomeness.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -2,6 +2,9 @@
 # $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 # require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 # set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+
+require "bundler/capistrano"
+
 set :application, "leihs-demo"
 
 set :scm, :git
