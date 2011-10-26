f8d1db8bea1226e95b115565f4f66d45ddffc792
Sebastian Pape <Sebastian@SPape.local>
2011-10-26 10:20:42 +0200

    minor changes

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -1,12 +1,14 @@
 # Be sure to restart your server when you modify this file.
 #
-# This file contains the settings for ActionController::ParametersWrapper
-# which will be enabled by default in the upcoming version of Ruby on Rails.
+# This file contains settings for ActionController::ParamsWrapper which
+# is enabled by default.
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-ActionController::Base.wrap_parameters :format => [:json]
+ActiveSupport.on_load(:action_controller) do
+  wrap_parameters format: [:json]
+end
 
 # Disable root element in JSON by default.
-if defined?(ActiveRecord)
-  ActiveRecord::Base.include_root_in_json = false
+ActiveSupport.on_load(:active_record) do
+  self.include_root_in_json = false # TODO do we want to activate it?
 end
