587af717e0b1ababa89d9853e85f5c317c5e2a54
Sebastian Pape <Sebastian@SPape.local>
2011-10-28 13:59:43 +0200

    Global Tips.js

---
+++ app/assets/javascripts/jquery/tips/tips.js
@@ -0,0 +1,59 @@
+/*
+ * Tips
+ *
+ * This script setups the qTips2 globaly
+ *
+ * @dependencies qTip2
+*/
+
+$(document).ready(function(){
+  Tips.setup();
+});
+
+var Tips = new Tips();
+
+function Tips() {
+  
+  this.firstShowTimeOut;
+  
+  this.setup = function() {
+    $.fn.qtip.defaults.position.my = 'bottom center';
+    $.fn.qtip.defaults.position.at = 'top center';
+    $.fn.qtip.defaults.position.viewport = $(window);
+    $.fn.qtip.defaults.show.delay = 350;
+    $.fn.qtip.defaults.show.solo = true;
+    $.fn.qtip.defaults.style.tip.width = 12;
+    $.fn.qtip.defaults.style.tip.height = 8;
+    $.fn.qtip.defaults.style.classes = "list_tip";
+    $.fn.qtip.defaults.show.effect = function(offset) {$(this).fadeIn();}
+    
+    $(".hastip").live("mouseenter", Tips.setupTip);
+  }
+  
+  this.setupTip = function(event) {
+    var _this = event.currentTarget;
+    $(_this).removeClass("hastip");
+    $(_this).find(".tip").after("<div class='tiptarget'></div")
+    $(_this).qtip({
+      content: {
+        text: $(_this).find(".tip")
+      },
+      position: {
+        target: $(_this).find(".tiptarget")
+      }
+    });
+    $(_this).qtip('render');
+    
+    Tips.firstShowTimeOut = window.setTimeout(function(){
+      $(_this).qtip('show');
+    }, 350);
+    
+    $(_this).bind("mouseleave", Tips.clearTimeOut);
+  }
+  
+  this.clearTimeOut = function(event) {
+    var _this = event.currentTarget;
+    $(_this).unbind("mouseleave", Tips.clearTimeOut);
+    window.clearTimeout(Tips.firstShowTimeOut);
+  }
+}
