f0f860c3e08b573fa4aa58088cbbedc04f1fe5ae
Sebastian Pape <Sebastian@SPape.local>
2011-11-14 16:01:34 +0100

    add datepicker_backend to i18n.js

--- app/assets/javascripts/i18n/i18n.js
+++ app/assets/javascripts/i18n/i18n.js
@@ -58,6 +58,26 @@ function i18n() {
       isRTL: false,
       showMonthAfterYear: false,
       yearSuffix: ''
+    },
+    
+    datepicker_backend: {
+      closeText: 'schliessen',
+      prevText: '&lt;',
+      nextText: '&gt;',
+      currentText: 'heute',
+      monthNames: ['Januar','Februar','März','April','Mai','Juni',
+      'Juli','August','September','Oktober','November','Dezember'],
+      monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
+      'Jul','Aug','Sep','Okt','Nov','Dez'],
+      dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+      dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+      dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+      weekHeader: 'Wo',
+      dateFormat: 'dd.mm.yy', //note jquery uses small m for months
+      firstDay: 1,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: ''
     }
   };
   
@@ -107,6 +127,26 @@ function i18n() {
       isRTL: false,
       showMonthAfterYear: false,
       yearSuffix: ''
+    },
+    
+    datepicker_backend: {
+      closeText: 'Done',
+      prevText: '&lt;',
+      nextText: '&gt;',
+      currentText: 'Today',
+      monthNames: ['January','February','March','April','May','June',
+      'July','August','September','October','November','December'],
+      monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+      weekHeader: 'Wk',
+      dateFormat: 'mm/dd/yy', //note jquery uses small m for months
+      firstDay: 0,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: ''
     }
   };
   
@@ -152,6 +192,26 @@ function i18n() {
       dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
       weekHeader: 'Wk',
       dateFormat: 'dd/mm/y', //note jquery uses small m for months
+      firstDay: 0,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: ''
+    },
+    
+    datepicke_backend: {
+      closeText: 'Done',
+      prevText: '&lt;',
+      nextText: '&gt;',
+      currentText: 'Today',
+      monthNames: ['January','February','March','April','May','June',
+      'July','August','September','October','November','December'],
+      monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+      weekHeader: 'Wk',
+      dateFormat: 'dd/mm/yy', //note jquery uses small m for months
       firstDay: 0,
       isRTL: false,
       showMonthAfterYear: false,
