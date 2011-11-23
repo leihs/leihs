3cecf25c9850cf62a95f320fd8d2e25e4c82df22
Ramón Cahenzli <rca@psy-q.ch>
2011-11-23 15:50:03 +0100

    Hoping to fix the migration, but not sure since this has changed in Rails 3.

--- db/migrate/20110117113700_remove_fk_indices.rb
+++ db/migrate/20110117113700_remove_fk_indices.rb
@@ -10,19 +10,33 @@ class RemoveFkIndices < ActiveRecord::Migration
     # AR seems to guess the wrong foreign key name, which it can't remove.
     sql = ContractLine.connection.execute("show create table contract_lines")
     schema = sql.fetch_row[1]
+    schema.gsub!("`","") # Remove those ugly backticks from around field names
     
+    constraints = schema.split("\n").select{|l|
+                                            l =~ /^\s+CONSTRAINT/
+                                           }
     keys_to_check = ["fk_contract_lines_contract_id", "fk_contract_lines_item_id", "fk_contract_lines_model_id", "fk_contract_lines_option_id"]
     
     keys_to_check.each do |k|
-      if schema.include?(k)
+      # The constraint exists in this table description -- so we can safely kill it. Otherwise
+      # MySQL throws a hissy fit.
+      unless constraints.select{|c|
+                            c =~ /^\s+CONSTRAINT #{k}/
+                           }.empty?
         execute "ALTER TABLE contract_lines DROP FOREIGN KEY #{k}"
-        execute "ALTER TABLE contract_lines DROP KEY #{k}"
-        field_name = k.gsub("fk_contract_lines_","")
-        change_table :contract_lines do |t|
-          t.index field_name
-        end
-      end
-    end
+        
+        # That last one is a special case
+        unless k == "fk_contract_lines_option_id"
+          execute "ALTER TABLE contract_lines DROP KEY #{k}" 
+          field_name = k.gsub("fk_contract_lines_","")
+          change_table :contract_lines do |t|
+            t.index field_name
+          end
+        end # /inner unless
+        
+      end # /outer unless
+      
+    end # /keys_to_check.each
   end
 
   def self.down
