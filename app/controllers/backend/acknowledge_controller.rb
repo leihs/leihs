256a13525e7d093890e949480a620122715fed64
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-12-12 18:11:36 +0100

    removing old code

--- app/controllers/backend/acknowledge_controller.rb
+++ app/controllers/backend/acknowledge_controller.rb
@@ -9,9 +9,6 @@ class Backend::AcknowledgeController < Backend::BackendController
     @orders = Order.sphinx_submitted.search params[:query], { :star => true, :page => params[:page], :per_page => $per_page,
                                                               :with => with }
 
-    @working_orders = Order.sphinx_submitted.search params[:query], { :star => true, :page => params[:page], :per_page => $per_page,
-                                                                      :without => {:backup_id => 0},
-                                                                      :with => with }
     respond_to do |format|
       format.html
       format.js { search_result_rjs(@orders) }
