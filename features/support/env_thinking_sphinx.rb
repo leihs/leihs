c66cffc3bf2e00d7b65c44533b166bbf9cfe36b8
Franco Sellitto <franco.sellitto@zhdk.ch>
2011-12-19 18:00:30 +0100

    removing cucumber environment support for thinking_sphinx

--- features/support/env_thinking_sphinx.rb
+++
@@ -1,5 +0,0 @@
-Cucumber::Rails::World.use_transactional_fixtures = false
-
-# http://freelancing-god.github.com/ts/en/testing.html
-require 'cucumber/thinking_sphinx/external_world'
-Cucumber::ThinkingSphinx::ExternalWorld.new
