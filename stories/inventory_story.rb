905bb169b0c263b21d5e9e5872e61e78a4439523
Ramón Cahenzli <rca@psy-q.ch>
2011-12-23 14:49:18 +0100

    This is old stuff that was used on the old rspec story runner. No longer necessary.

--- stories/inventory_story.rb
+++
@@ -1,9 +0,0 @@
-dir = File.dirname(__FILE__)
-
-require dir + "/helper"
-
-FileUtils.remove_dir(dir + "/../index/test", true)
-
-with_steps_for(:inventory) do
-  run_local_story "./text/inventory_story", :type => RailsStory
-end
--- stories/order_backup_story.rb
+++
@@ -1,9 +0,0 @@
-dir = File.dirname(__FILE__)
-
-require dir + "/helper"
-
-FileUtils.remove_dir(dir + "/../index/test", true)
-
-with_steps_for(:order_backup, :order, :login) do
-  run_local_story "./text/order_backup_story", :type => RailsStory
-end
