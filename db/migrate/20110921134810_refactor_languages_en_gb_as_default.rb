75c4910489b9f98c719e264f01555c731252f508
Sebastian Pape <email@sebastianpape.com>
2012-03-05 17:23:06 +0100

    missing rename of require path (Factory -> LeihsFactory)

--- db/migrate/20110921134810_refactor_languages_en_gb_as_default.rb
+++ db/migrate/20110921134810_refactor_languages_en_gb_as_default.rb
@@ -3,7 +3,7 @@ class RefactorLanguagesEnGbAsDefault < ActiveRecord::Migration
     
     change_column_default :users, :language_id, nil
 
-    require("#{Rails.root}/lib/factory.rb")
+    require("#{Rails.root}/lib/leihs_factory.rb")
     LeihsFactory.create_default_languages
   end
 end
