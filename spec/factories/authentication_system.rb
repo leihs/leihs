48e992297c496bc4059de2ebe50bec27093b6ea0
Sebastian Pape <email@sebastianpape.com>
2012-03-05 16:15:51 +0100

    Fix initalize error (was caused by a factory statement that was not inside of a block)

--- spec/factories/authentication_system.rb
+++ spec/factories/authentication_system.rb
@@ -3,8 +3,7 @@ FactoryGirl.define do
   factory :authentication_system do
     name Faker::Lorem.words(1).join.capitalize
     class_name { name }
-    is_default AuthenticationSystem.find_by_is_default(1).blank? ? 1 : 0
+    is_default { AuthenticationSystem.find_by_is_default(1).blank? ? 1 : 0 }
     is_active 1
   end
-
 end
