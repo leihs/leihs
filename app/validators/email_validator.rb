b44cb8afc39f5d8aeebd663275d486f0a9f21e78
Sebastian Pape <email@sebastianpape.com>
2012-03-05 16:14:41 +0100

    Fix E-Mail Validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,8 +1,12 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    unless value.to_s.empty? 
-      valid = value =~ /^[\[_a-z0-9\-\]]+(\.[\[\]_A-Za-z0-9\-]+)*@[a-z0-9]+(\.[_A-Za-z0-9\-]+)*(\.[A-Za-z]{2,})$/
-      errors.add(:name, "#{value} is not a valid email address") unless valid
+    
+    unless value.to_s.empty? # email can be blank 
+      
+      # validate email syntax
+      valid = (value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i) ? true : false
+      record.errors.add("#{value} is not a valid email address") unless valid
+      
     end
   end
 end
