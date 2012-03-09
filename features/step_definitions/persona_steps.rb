ab50648fa5936159f9a05b285f4513270e9ddaf3
Sebastian Pape <email@sebastianpape.com>
2012-03-09 16:27:37 +0100

    enable persona steps

--- features/step_definitions/persona_steps.rb
+++ features/step_definitions/persona_steps.rb
@@ -1,12 +1,13 @@
 Given /^personas are loaded$/ do
 
-  # Persona.create("Ramon") # Ramon should created first, he is setting up the Application
-  # Persona.create("Mike") 
-  # Persona.create("Pius") 
-  # Persona.create("Normin") 
+  Persona.create("Ramon") # Ramon should created first, he is setting up the Application
+  Persona.create("Mike") 
+  Persona.create("Pius") 
+  Persona.create("Normin") 
 
 end
 
-Given /^I am '(\w+)'$/ do |persona_name|
+Given /^I am "([^"]*)"$/ do |persona_name|
   step "I am logged in as '%s' with password 'password'" % persona_name.downcase
 end
+
