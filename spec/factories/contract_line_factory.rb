778eec0b95162dc0e7eb29c164b2ee1830ae323b
Sebastian Pape <email@sebastianpape.com>
2012-03-26 10:12:50 +0200

    fix contract line factory -> there is no belongs to model, so set model_id explicitly

--- spec/factories/contract_line_factory.rb
+++ spec/factories/contract_line_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :contract_line do
     contract { Factory :contract }
-    model { Factory :model }
+    model_id { (Factory :model).id }
     quantity 1
     start_date { Date.today }
     end_date { Date.tomorrow }
