1d31b6119e100da528e1f4e1dbcda3ac88e134e7
Sebastian Pape <email@sebastianpape.com>
2012-03-26 17:03:35 +0200

    go on with hand over testing

--- features/step_definitions/backend/hand_over/sign_contract_steps.rb
+++ features/step_definitions/backend/hand_over/sign_contract_steps.rb
@@ -1,6 +1,12 @@
 When /^I open a hand over$/ do
-  step 'I go to the home page'
+  @ip = @user.managed_inventory_pools.first
   binding.pry
+  @customer = @ip.users.all.select {|x| x.orders.size > 0}.first
+  @order = @customer.order.first
+  
+  visit backend_inventory_pool_path @user.managed_inventory_pools.first
+  find(".hand_over.line .button", :text => "Hand Over").click
+  page.has_css?("#hand_over", :visible => true)
 end
 
 When /^I select some lines$/ do
--- features/support/env.rb
+++ features/support/env.rb
@@ -34,6 +34,7 @@ ActionController::Base.allow_rescue = false
 begin
   require 'database_cleaner'
   require 'database_cleaner/cucumber'
+  DatabaseCleaner.clean_with :truncation
   DatabaseCleaner.strategy = :transaction
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
