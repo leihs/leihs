ee97ddc459c233bb45fc80a9f5d35578e38ce2d4
Sebastian Pape <email@sebastianpape.com>
2012-03-26 09:34:54 +0200

    starting hand over controller spec

---
+++ spec/controllers/backend/hand_over_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Backend::HandOverController do
+  render_views
+  
+  describe "assigning an inventory_code" do
+    
+    context "selecting an item_line directly" do
+        
+    end
+    
+    context "without selecting an item_line" do
+        
+    end
+  end
+end
