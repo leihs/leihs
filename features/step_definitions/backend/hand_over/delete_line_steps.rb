6efb3f601e79b07c3a1f274792b1b776918e4f72
Sebastian Pape <email@sebastianpape.com>
2012-04-05 10:03:23 +0200

    delete line steps

---
+++ features/step_definitions/backend/hand_over/delete_line_steps.rb
@@ -0,0 +1,7 @@
+When /^I delete a line$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^this line is deleted$/ do
+  pending # express the regexp above with the code you wish you had
+end
