76d8fb8bebaadd3027f9d2b133dbfa5e093817b6
Sebastian Pape <email@sebastianpape.com>
2012-04-09 13:10:22 +0200

    Finish steps for #27077331: Delete a single contract line

--- features/step_definitions/backend/hand_over/delete_line_steps.rb
+++ features/step_definitions/backend/hand_over/delete_line_steps.rb
@@ -1,7 +1,13 @@
 When /^I delete a line$/ do
-  pending # express the regexp above with the code you wish you had
+  @line = @customer.contracts.unsigned.first.lines.first
+  @line_element = find(".line", :text => @line.model.name)
+  @line_element.find(".multibutton .trigger").click
+  @line_element.find(".button", :text => "Delete").click
+  wait_until{
+    all(".line", :text => @line.model.name).size == 0
+  }
 end
 
 Then /^this line is deleted$/ do
-  pending # express the regexp above with the code you wish you had
+  lambda {@line.reload}.should raise_error(ActiveRecord::RecordNotFound) 
 end
