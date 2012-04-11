2022b76f2780cf38c7643c7838638cee13d5736f
Sebastian Pape <email@sebastianpape.com>
2012-04-11 17:23:04 +0200

    add_item for acknowledge tests

--- features/backend/acknowledge/add_item.feature
+++ features/backend/acknowledge/add_item.feature
@@ -9,13 +9,23 @@ Feature: Add Item during acknowledge process
     Given I am "Pius"
      When I open an order for acknowledgement
       And I add a model by typing in the inventory code of an item of that model to the quick add
-     Then this model is added to the order 
+     Then the model is added to the order 
     
   @javascript
-  Scenario: Autocompletion of the quick add input (by inventory code)
+  Scenario: Autocompletion of the quick add input by inventory code
     Given I am "Pius"
      When I open an order for acknowledgement
       And I start to type the inventory code of an item
+      And I wait until the autocompletion is loaded
+     Then I already see possible matches of models
+     When I select one of the matched models
+     Then the model is added to the order
+    
+  @javascript
+  Scenario: Autocompletion of the quick add input by model name
+    Given I am "Pius"
+     When I open an order for acknowledgement
+      And I start to type the name of a model
       And I wait until the autocompletion is loaded
      Then I already see possible matches of models
      When I select one of the matched models
--- features/step_definitions/backend/acknowledge/add_item_steps.rb
+++ features/step_definitions/backend/acknowledge/add_item_steps.rb
@@ -1,15 +1,40 @@
-When /^I edit an order for acknowledgment$/ do
-  visit backend_inventory_pool_acknowledge_path(@user.managed_inventory_pools.first, 
-                                                @user.managed_inventory_pools.first.orders.first)
+When /^I add a model by typing in the inventory code of an item of that model to the quick add$/ do
+  @item = @ip.items.first
+  find("#add_item #quick_add").set @item.inventory_code
+  find("#add_item .button[type=submit]").click
+  wait_until {
+    all("#add_item .loading").size == 0
+  }
 end
 
-When /^I add an item through the quick add item field$/ do
-  fill_in("quick_add", :with =>  @user.managed_inventory_pools.first.items.first.serial_number)
-  
-  pending
+When /^I start to type the inventory code of an item$/ do
+  @item = @ip.items.first
+  find("#add_item").fill_in 'code', :with => @item.inventory_code[0..2] 
 end
 
-Then /^the item is added to the order$/ do
-  pending # express the regexp above with the code you wish you had
+When /^I wait until the autocompletion is loaded$/ do
+  wait_until {
+    all("#add_item .loading").size == 0 and find(".ui-autocomplete")
+  }
 end
 
+Then /^I already see possible matches of models$/ do
+  find(".ui-autocomplete").should have_content @item.model.name
+end
+
+When /^I select one of the matched models$/ do
+  find(".ui-autocomplete").find("a", :text => @item.model.name)
+  wait_until {
+    all("#add_item .loading").size == 0
+  }
+end
+
+Then /^the model is added to the order$/ do
+  page.should have_content @item.model.name
+  @order.models.include? @item.model
+end
+
+When /^I start to type the name of a model$/ do
+  @item = @ip.items.first
+  find("#add_item").fill_in 'code', :with => @item.model.name[0..3] 
+end
