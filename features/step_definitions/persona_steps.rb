d5791553a254fd88a5b42be740ff41bbbdeadcfa
Sebastian Pape <email@sebastianpape.com>
2012-05-04 11:48:46 +0200

    implement persona step "i am" for language: DE

--- features/step_definitions/persona_steps.rb
+++ features/step_definitions/persona_steps.rb
@@ -3,3 +3,7 @@ Given /^I am "([^"]*)"$/ do |persona_name|
   step 'I login'
 end
 
+Angenommen /^man ist "([^"]*)"$/ do |persona_name|
+  step "I am logged in as '%s' with password 'password'" % persona_name.downcase
+  step 'I login'
+end
