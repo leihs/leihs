d4570341d72c871298c7167943491e7801164148
Ramón Cahenzli <rca@psy-q.ch>
2012-05-08 15:18:47 +0200

    Disallow en-GB

--- config/initializers/gettext.rb
+++ config/initializers/gettext.rb
@@ -1,5 +1,5 @@
 #rails3#
 FastGettext.add_text_domain 'leihs', :path => 'locale'#, :type => :po
-FastGettext.default_available_locales = ['en-US', 'en-GB', 'de-CH', 'es', 'fr', 'gsw-CH'] #all you want to allow
+FastGettext.default_available_locales = ['en-US', 'de-CH', 'es', 'fr', 'gsw-CH'] #all you want to allow
 FastGettext.default_text_domain = 'leihs'
 
