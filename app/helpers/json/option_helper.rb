f51dc124856869103cde8ef59e45e17559a2d69d
Franco Sellitto <franco.sellitto@zhdk.ch>
2012-05-29 17:24:06 +0200

    (wip) inventory list with model, items and options. TODO: packages

--- app/helpers/json/option_helper.rb
+++ app/helpers/json/option_helper.rb
@@ -3,10 +3,16 @@ module Json
 
     def hash_for_option(option, with = nil)
       h = {
+        type: option.class.to_s.underscore,
         id: option.id,
-        name: option.name,
-        type: option.class.to_s.underscore
+        name: option.name
       }
+
+      if with ||= nil
+        [:inventory_code, :price].each do |k|
+          h[k] = option.send(k) if with[k]
+        end
+      end
       
       h
     end
