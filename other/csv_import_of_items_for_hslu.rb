6317e02c4b9f5a4cf5d6c0a9af0839f42d44f24c
Ramón Cahenzli <rca@psy-q.ch>
2012-11-28 14:35:51 +0100

    Example CSV for HSLU

--- other/csv_import_of_items_for_hslu.rb
+++ other/csv_import_of_items_for_hslu.rb
@@ -3,23 +3,31 @@
 # require 'doc/csv_import_of_items'
 
 
+
+
+# Example of some correct CSV lines for this (the separators are tabs):
+# inventory_code	inventory_pool	owner	serial_number	model_name	categories	supplier	model_manufacturer	location	note
+#5049	Videowerkstatt Sentimatt	Design & Kunst	FN 1209601	Thomson VTH 6050	Player / Recorder (Entsorgt)|	Bild+Ton	Thomson	125	FernbedienungN2QAKB0000003
+#5065	Videowerkstatt Sentimatt	Design & Kunst	2B1651986	Standard SR - 8900	Player DVD (entsorgt 05)|		Standard	125	- 1 Fernbedienung
+#5060	Videowerkstatt Sentimatt	Design & Kunst	JOHT00188	Panasonic NV- DV2000 4	Player / Recorder fehlt 04|		Panasonic	126	- 1 Fernbedienung
+#5069	Videowerkstatt Sentimatt	Design & Kunst	K2HT00005	Panasonic NV- DV2000, 5	Player / Recorder|		Panasonic	126	- 1 Fernbedienung
+#5084	Videowerkstatt Sentimatt	Design & Kunst	22026	SONY GV-HD700E	Player / Recorder HDV|		SONY	44b	- Stromadapter AC-L100|- 1 Akku NP-F570|- Fernbedinung|- Manual|- RGB Kabel|- USB Kabel|- Video Kabel
+#5085	Videowerkstatt Sentimatt	Design & Kunst	22276	SONY GV-HD700E	Player / Recorder HDV|		SONY	44b	- Stromadapter AC-L100|- 1 Akku NP-F570|- Fernbedinung|- Manual|- RGB Kabel|- USB Kabel|- Video Kabel
+
+
 require 'faster_csv'
 @failures = 0
 @successes = 0
+@report = ""
 
 def create_item(model_name, inventory_code, serial_number, manufacturer, 
                 category, accessory_string, note, building_string, room_string, owner, inventory_pool)
   
 
   if model_name.blank?
-    puts "Can't create item with a blank name."
+    puts "Can't create item with a blank model name."
   else
 
-    m = Model.find_by_name(model_name)
-    if m.nil?
-      m = create_model(model_name, category, manufacturer, accessory_string)
-    end
-   
     if owner
       owner_ip = InventoryPool.find_or_create_by_name(owner)
     end
@@ -28,7 +36,7 @@ def create_item(model_name, inventory_code, serial_number, manufacturer,
     end
 
     i = Item.new
-    i.model = m
+    i.model = create_model(model_name, category, manufacturer, accessory_string)
     i.inventory_code = inventory_code
     i.serial_number = serial_number
     i.note = note
@@ -44,37 +52,33 @@ def create_item(model_name, inventory_code, serial_number, manufacturer,
       puts i.inspect
     else
       @failures += 1
-      puts "Could not import item #{inventory_code}"
+      @report += "Could not import item #{inventory_code}\n"
     end
-    
-    puts "-----------------------------------------"
-    puts "DONE"
-    puts "#{@successes} successes, #{@failures} failures"
-    puts "-----------------------------------------"
   end  
 end
 
 def create_model(name, category, manufacturer, accessory_string)
-  m = Model.create(:name => name, :manufacturer => manufacturer)
+  m = Model.find_by_name(name)
+  if m.nil?
+    m = Model.create(:name => name, :manufacturer => manufacturer)
 
-  unless category.blank?
-    category.split("|").each do |cat_string|
-      unless cat_string.blank?
-        c = Category.find_or_create_by_name(cat_string)
-        m.categories << c
+    unless category.blank?
+      category.split("|").each do |cat_string|
+        unless cat_string.blank?
+          c = Category.find_or_create_by_name(cat_string)
+          m.categories << c
+        end
       end
     end
-  end
-  
-  unless accessory_string.blank?  
-    accessory_string.split(",").each do |string|
-      acc = Accessory.create(:name => string.strip)
-      m.accessories << acc
+    
+    unless accessory_string.blank?  
+      accessory_string.split("|").each do |string|
+        acc = Accessory.create(:name => string.strip)
+        m.accessories << acc
+      end
     end
+    m.save
   end
-  
-  m.save
-
   return m
 end
 
@@ -117,26 +121,27 @@ def run_import(path)
   lines_to_import = FasterCSV.open(path, :col_sep => "\t", :quote_char => "\"", :headers => true) 
   lines_to_import.each do |line|
 
-  item = line
-  #  split_line = line.split("\t")
-  #  item = {}
-  #  item["model_name"] = split_line[4]
-  #  item["inventory_code"] = split_line[0]
-  #  item["serial_number"] = split_line[3]
-  #  item["model_manufacturer"] = split_line[7]
-  #  item["category"] = split_line[5]
-  #  item["note"] = "" 
-  #  item["note"] = split_line[9] if split_line[9]
-  #  item["building_string"] = split_line[8]
-  #  item["room_string"] = ""
-  #  item["owner"] = split_line[2]
-  #  item["inventory_pool"] = split_line[1]
+    item = line
+    #  split_line = line.split("\t")
+    #  item = {}
+    #  item["model_name"] = split_line[4]
+    #  item["inventory_code"] = split_line[0]
+    #  item["serial_number"] = split_line[3]
+    #  item["model_manufacturer"] = split_line[7]
+    #  item["category"] = split_line[5]
+    #  item["note"] = "" 
+    #  item["note"] = split_line[9] if split_line[9]
+    #  item["building_string"] = split_line[8]
+    #  item["room_string"] = ""
+    #  item["owner"] = split_line[2]
+    #  item["inventory_pool"] = split_line[1]
+
 
     create_item(item["model_name"],
                 item["inventory_code"],
                 item["serial_number"],
                 item["model_manufacturer"],
-                item["category"],
+                item["categories"],
                 item["note"],
                 item["note"],
                 item["building_string"],
@@ -144,6 +149,14 @@ def run_import(path)
                 item["owner"],
                 item["inventory_pool"])
   end
+
+  puts "-----------------------------------------"
+  puts "DONE"
+  puts "#{@successes} successes, #{@failures} failures"
+  puts "-----------------------------------------"
+  puts "#{@report}"
+  puts "-----------------------------------------"
+
 end
 
 
