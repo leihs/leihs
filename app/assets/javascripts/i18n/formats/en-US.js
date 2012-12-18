47de045c1aded259955edb99236fe4ed63fdb6ff
Ramón Cahenzli <rca@psy-q.ch>
2012-12-18 16:12:40 +0100

    Date formats for English (UK and US)

--- app/assets/javascripts/i18n/formats/en-US.js
+++ app/assets/javascripts/i18n/formats/en-US.js
@@ -2,15 +2,15 @@
 (function() {
 
   i18n.date = {
-    L: "DD.MM.YYYY",
-    XL: "dddd DD.MM.YYYY",
-    XXL: "DD.MM.YYYY LT",
-    XXXL: "dddd DD.MM.YYYY LT",
-    XS: 'DD.MM.YY'
+    L: "MM/DD/YYYY",
+    XL: "dddd MM/DD/YYYY",
+    XXL: "MM.DD.YYYY LT",
+    XXXL: "dddd MM/DD/YYYY LT",
+    XS: 'MM/DD/YY'
   };
 
   i18n.datepicker = {
-    L: "dd.mm.yy"
+    L: "mm/dd/yy"
   };
 
   i18n.months = {
@@ -24,7 +24,7 @@
     trunc: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
   };
 
-  i18n.time = "H:mm U\\hr";
+  i18n.time = "hh:mm";
 
   i18n.meridiem = {
     AM: 'AM',
@@ -60,7 +60,7 @@
 
   i18n.number = {
     decimal: ".",
-    thousand: "'"
+    thousand: ","
   };
 
 }).call(this);
