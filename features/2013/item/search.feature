1af913e827932ca08727942f9764cad6be462c5c
Matus Kmit <matus.kmit@zhdk.ch>
2013-01-24 17:42:37 +0100

    RSpec for item search moved to cucumber.

---
+++ features/2013/item/search.feature
@@ -0,0 +1,11 @@
+Feature: Item search
+
+  Scenario: Search in properties' fields
+    Given there are some items
+     When I search for a text not present anywhere
+     Then there are no items found
+     When I fetch a random item
+      And I store some text as a value to some new property in this item
+      And I search for the same text I stored
+     Then there is one item found
+      And the item found is the one with the new property
---
+++ features/step_definitions/2013/item_steps.rb
@@ -0,0 +1,39 @@
+Given /^there are some items$/ do
+  3.times do
+    FactoryGirl.create :item
+  end
+end
+
+When /^I search for a text not present anywhere$/ do
+  @search_string = "ABC-123-@$!"
+  @search_result = Item.search(@search_string)
+end
+
+Then /^there are no items found$/ do
+  step "there are 0 items found"
+end
+
+Then /^there are (\d+) items found$/ do |n|
+  @search_result.count.should == n.to_i
+end
+
+When /^I fetch a random item$/ do
+  @fetched_item = Item.order("rand()").first
+end
+
+When /^I store some text as a value to some new property in this item$/ do
+  @fetched_item.properties[:mac_address] = @search_string
+  @fetched_item.save
+end
+
+Then /^I search for the same text I stored$/ do
+  @search_result = Item.search(@search_string)
+end
+
+Then /^there is one item found$/ do
+  step "there are 1 items found"
+end
+
+Then /^the item found is the one with the new property$/ do
+  @search_result.first.should eql @fetched_item
+end
