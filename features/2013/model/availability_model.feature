816361d1f5096c5e8959aee5403f2103cead587a
Matus Kmit <matus.kmit@zhdk.ch>
2013-02-11 13:39:18 +0100

    Moved rspec to cucumber for model availability test

--- features/2013/model/availability_model.feature
+++ features/2013/model/availability_model.feature
@@ -6,23 +6,25 @@ Feature: Availability
     Given personas existing
 
   Scenario: All items are available if there are no running reservations
-    Given pending rspec for: All items are available if there are no running reservations
-
+    Given pending cucumber for: All items are available if there are no running reservations
 
   Scenario: All lines are available
-    Given pending rspec for: All lines are available
+    Given pending cucumber for: All lines are available
 
+  Scenario: The quantity of items for users and models
+    Test is performed for all personas
 
-  Scenario: The quantity of items for users
-
+    Given list of all available models
+    And list of all users
+    When the quantity of items of a user for a specific model is retrieved
+    And the quantity of items of a model for a specific user is retrieved
+    Then these quantities must be equal
 
   Scenario: Total borrowable items
-    Given pending rspec for: Total borrowable items
-
+    Given pending cucumber for: Total borrowable items
 
   Scenario: Scoped by inventory pool
-
+    Given pending cucumber for: Scoped by inventory pool
 
   Scenario: The maximum quantity available for users
-    Given pending rspec for: The maximum quantity available for users
-
+    Given pending cucumber for: The maximum quantity available for users
--- features/step_definitions/2013/model_steps.rb
+++ features/step_definitions/2013/model_steps.rb
@@ -1,15 +1,67 @@
-Given /^pending rspec for: All items are available if there are no running reservations$/ do
-  pending # express the regexp above with the code you wish you had
+Given /^pending cucumber for: All items are available if there are no running reservations$/ do
+  pending
+  #@models_without_reservations.all? do |model|
+  #  InventoryPool.all.all? do |ip|
+  #    aq = model.availability_in(ip).changes.select {|x| x.date >= Date.today and x.date <= Date.today }.flat_map(&:quantities)
+  #  end
+  #end.should be_true
 end
 
-Given /^pending rspec for: All lines are available$/ do
-  pending # express the regexp above with the code you wish you had
+Given /^pending cucumber for: All lines are available$/ do
+  pending
+  #Visit.all.flat_map(&:lines).all?(&:available?).should be_true
 end
 
-Given /^pending rspec for: Total borrowable items$/ do
-  pending # express the regexp above with the code you wish you had
+When /^list of all available models$/ do
+  @models = Model.all
+  # @models_with_reservations = Visit.all.flat_map{|x| x.lines.flat_map(&:model) }.uniq
+  # @models_without_reservations = @all_models - @models_with_reservations
 end
 
-Given /^pending rspec for: The maximum quantity available for users$/ do
-  pending # express the regexp above with the code you wish you had
+When /^list of all users$/ do
+  @users = User.all
+end
+
+When /^the quantity of items of a user for a specific model is retrieved$/ do
+  @quantities_1 = []
+  @users.each do |user|
+    @models.each do |model|
+      @quantities_1 << user.items.where(:model_id => model).count
+    end
+  end
+end
+
+When /^the quantity of items of a model for a specific user is retrieved$/ do
+  @quantities_2 = []
+  @users.each do |user|
+    @models.each do |model|
+      @quantities_2 << model.items.where(:inventory_pool_id => user.inventory_pools).count
+    end
+  end
+end
+
+Then /^these quantities must be equal$/ do
+  @quantities_1.should eq @quantities_2
+end
+
+Given /^pending cucumber for: Total borrowable items$/ do
+  pending
+  # NOTE: DONT EXPECT THINGS THAT YOU MIGHT KNOW FROM THE PERSONAS !!!
+  # HINT: infact they can change !!!
+  # model = Model.find_by_name("Sharp Beamer")
+  # model.items.count.should == 3
+  # model.total_borrowable_items_for_user(@normin).should == 3
+end
+
+Given /^pending cucumber for: Scoped by inventory pool$/ do
+  @normin = Persona.get "Normin"
+  model = Model.find_by_name("Sharp Beamer")
+  ip_a = InventoryPool.find_by_name "A-Ausleihe"
+  model.total_borrowable_items_for_user(@normin, ip_a).should == 2
+  ip_b = InventoryPool.find_by_name "IT-Ausleihe"
+  model.total_borrowable_items_for_user(@normin, ip_b).should == 1
+end
+
+Given /^pending cucumber for: The maximum quantity available for users$/ do
+  pending
 end
