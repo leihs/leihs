4fb522d65747fc97d63bc32343845dfc47014ffe
Sebastian Pape <email@sebastianpape.com>
2013-03-06 09:24:11 +0100

    Revert "[CI] try tentaculus"
    This reverts commit c6dd727d49ac9fc5921f023c8e451529c05fcd92.

--- lib/ci/post_job_shell_commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash --login
-
-mkdir -p $WORKSPACE/tmp/capybara&#xd;
-rm -rf $WORKSPACE/log &amp;&amp; mkdir -p $WORKSPACE/log&#xd;
-rm -f $WORKSPACE/tmp/*.mysql&#xd;
-rm -f $WORKSPACE/tmp/*.sql&#xd;
-mkdir -p $WORKSPACE/tmp/html&#xd;
-command -v rvm &gt;/dev/null 2&gt;&amp;1 &amp;&amp;  rvm use 1.9.3&#xd;
-bundle exec rake madek:test:drop_ci_dbs --trace
