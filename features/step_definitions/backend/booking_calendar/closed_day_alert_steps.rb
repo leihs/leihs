d72058020dfe46f3c85cf6c2ff3b39f7c0785e7c
Matus Kmit <matus.kmit@zhdk.ch>
2013-03-25 16:54:50 +0100

    Fix test for closed_day_alert.feature

--- features/step_definitions/backend/booking_calendar/closed_day_alert_steps.rb
+++ features/step_definitions/backend/booking_calendar/closed_day_alert_steps.rb
@@ -1,6 +1,8 @@
 When /^I pick a closed day for beeing the "(.*?)"$/ do |date_target|
   next_closed_day = nil
   date = Date.today
+  if all("td[data-date='#{date}']").empty? then date = Date.new(date.year, date.month + 1) end
+
   while next_closed_day.nil?
     next_date = @ip.next_open_date date+1.day
     next_closed_day = (next_date - 1.day) if (next_date-date).to_i > 1
