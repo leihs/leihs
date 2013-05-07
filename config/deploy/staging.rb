b7e5e0674b071caefbbead0a875956c427541bc4
Ramón Cahenzli <rca@psy-q.ch>
2013-05-07 12:46:30 +0200

    Staging is locked to the fixed alpha 3.1

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,7 +10,7 @@ set :application, "leihs-test"
 
 set :scm, :git
 set :repository,  "git://github.com/zhdk/leihs.git"
-set :branch, "next"
+set :branch, "3.0.0-alpha.3.1"
 set :deploy_via, :remote_cache
 
 set :db_config, "/home/leihs/#{application}/database.yml"
