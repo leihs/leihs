75db1467d96b377a422381fd78b8796f31fdc913
Sebastian Pape <email@sebastianpape.com>
2013-06-18 10:16:08 +0200

    [FIX CI] reject.feature

--- features/step_definitions/backend/acknowledge/reject_steps.rb
+++ features/step_definitions/backend/acknowledge/reject_steps.rb
@@ -5,14 +5,17 @@ end
 
 When /^I reject an order$/ do
   @order = @ip.orders.submitted.first
-  @order_line = find(".order .list .line", :text => /#{@order.purpose.description[0..25]}/)
+  find(".toggle .text").click
+  @order_line = find(".order .list .line[data-id='#{@order.id}']")
   @order_line.find(".actions .trigger").click
   @order_line.find(".actions .button", :text => /(Reject|Ablehnen)/).click
 end
 
 Then /^I see a summary of that order$/ do
   wait_until { find(".dialog") }
-  find(".dialog .purpose").should have_content @order.purpose.description[0..25]
+  unless @order.purpose.description.nil?
+    find(".dialog .purpose").should have_content @order.purpose.description[0..25]
+  end
 end
 
 Then /^I can write a reason why I reject that order$/ do
@@ -24,10 +27,10 @@ When /^I reject the order$/ do
 end
 
 Then /^the order is rejected$/ do
-  wait_until { all(".order .list .line", :text => /#{@order.purpose.description[0..25]}/).size == 0 }
+  wait_until { all(".order .list .line[data-id='#{@order.id}']").size == 0 }
   @order.reload.status_const.should == Order::REJECTED 
 end
 
 Then /^the counter of that list is updated/ do
-  @order_line.find(:xpath, "../..").find(".badge").text.to_i.should == @ip.orders.submitted.count
+  find(".order .list .line").find(:xpath, "../..").find(".badge").text.to_i.should == @ip.orders.submitted.count
 end
