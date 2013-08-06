e7719e1f11a26227a2d449be8d01159553819692
Ramón Cahenzli <rca@psy-q.ch>
2013-08-06 14:49:03 +0200

    Revamped bootstrap, now uses RVM and Passenger.

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install --assume-yes curl make build-essential git libxslt-dev libcairo2-dev libmysqlclient-dev libxml2-dev libreadline6-dev libssl-dev libyaml-dev autoconf libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+# The Ruby version you want to install and use for this Vagrant box
+RUBY_VERSION=ruby-1.9.3-p448
+PASSENGER_VERSION=3.0.9
 
+
+##########  Packages and base requirements
+
+# Always take this information from the official wiki: https://github.com/zhdk/leihs/wiki
+# NOTE: Do not install mysql-server here, as that would ask for a password using a dialog, and we can't do that
+# on the Vagrant shell.
+sudo apt-get install --assume-yes build-essential make libxslt-dev libcairo2-dev libmysqlclient-dev libxml2-dev curl make build-essential git libxslt-dev libcairo2-dev libmysqlclient-dev libxml2-dev libreadline6-dev libssl-dev libyaml-dev autoconf libgdbm-dev libncurses5-dev automake libtool bison libffi-dev imagemagick libcurl4-openssl-dev apache2-prefork-dev apache2 mysql-client libmagickwand-dev
 
 # Without this, installing MySQL will prompt for a MySQL root password, but we can't
 # enter one in this noninteractive shell. With the noninteractive option set, the
@@ -9,21 +18,60 @@ sudo apt-get install --assume-yes curl make build-essential git libxslt-dev libc
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes mysql-server
 
 
-# Check if the .rbenv directory already exists, because we might not have to install
-# rbenv in such case.
-if [ ! -d /home/vagrant/.rbenv ]; then
-        git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
-        git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
-        echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bash_profile
-        echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bash_profile
-        chown -R vagrant /home/vagrant/.rbenv
-        chown -R vagrant /home/vagrant/.bash_profile
+
+########## RVM, Ruby and bundler
+
+# Only RVM is supported by Passenger. Cannot use rbenv.
+# Also, only RVM has a good system-wide install.
+if [ ! -f /usr/local/bin/rvm ]; then
+        sudo curl -L https://get.rvm.io | bash -s stable
+        sudo -i rvm install $RUBY_VERSION
+fi
+
+# Install or update Bundler
+sudo -i "gem install bundler"
+
+
+########## Passenger and Apache configuration
+
+# Install Phusion Passenger
+if [ ! -f /etc/apache2/mods-available/passenger.load ]; then
+        sudo -i "gem install passenger -v $PASSENGER_VERSION"
+        sudo -i "passenger-install-apache2-module"
+
+
+PASSENGER=$(cat <<ENDPASSENGER
+   LoadModule passenger_module /usr/local/rvm/gems/$RUBY_VERSION/gems/passenger-$PASSENGER_VERSION/ext/apache2/mod_passenger.so
+   PassengerRoot /usr/local/rvm/gems/$RUBY_VERSION/gems/passenger-$PASSENGER_VERSION
+   PassengerRuby /usr/local/rvm/wrappers/$RUBY_VERSION/ruby
+ENDPASSENGER
+)
+        sudo echo "$PASSENGER" >> /etc/apache2/mods-available/passenger.load
+        sudo a2enmod passenger
+        sudo service apache2 restart
+
 fi
 
 
-# su vagrant because by default it runs as root.
-# bash -l because that creates a login shell, login shells process the .bash_profile files, non-login
-# shells do not.
-# TODO: Check if the required ruby version is already installed and skip this expensive
-# step'.
-su vagrant -c "bash -l -c 'rbenv rehash && rbenv install 1.9.3-p286 && rbenv global 1.9.3-p286'"
+# Enable the leihs virtual host
+if [ ! -f /etc/apache2/sites-available/leihs ]; then
+VHOST=$(cat <<ENDVHOST
+<VirtualHost *:80>
+   ServerName leihs.local
+   DocumentRoot /vagrant/public/
+
+   <Directory /vagrant/public>
+      AllowOverride all
+      Options -MultiViews
+   </Directory>
+
+</VirtualHost>
+ENDVHOST
+)
+
+        echo "$VHOST" >> /etc/apache2/sites-available/leihs
+        sudo a2ensite leihs
+        sudo service apache2 restart
+fi
+
+
