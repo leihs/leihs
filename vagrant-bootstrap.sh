26bd856afb123301b7daa12e114dd4a8499820e3
Ramón Cahenzli <rca@psy-q.ch>
2013-08-06 16:03:02 +0200

    Vagrant, sudo, RVM and non-interactive shells do not play well with each other. Splitting the setup onto an interactive shell.

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# The Ruby version you want to install and use for this Vagrant box
-RUBY_VERSION=ruby-1.9.3-p448
-PASSENGER_VERSION=3.0.9
-
 
 ##########  Packages and base requirements
 
@@ -18,60 +14,22 @@ sudo apt-get install --assume-yes build-essential make libxslt-dev libcairo2-dev
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes mysql-server
 
 
-
 ########## RVM, Ruby and bundler
 
 # Only RVM is supported by Passenger. Cannot use rbenv.
 # Also, only RVM has a good system-wide install.
 if [ ! -f /usr/local/bin/rvm ]; then
         sudo curl -L https://get.rvm.io | bash -s stable
-        sudo -i rvm install $RUBY_VERSION
-fi
-
-# Install or update Bundler
-sudo -i "gem install bundler"
-
-
-########## Passenger and Apache configuration
-
-# Install Phusion Passenger
-if [ ! -f /etc/apache2/mods-available/passenger.load ]; then
-        sudo -i "gem install passenger -v $PASSENGER_VERSION"
-        sudo -i "passenger-install-apache2-module"
-
-
-PASSENGER=$(cat <<ENDPASSENGER
-   LoadModule passenger_module /usr/local/rvm/gems/$RUBY_VERSION/gems/passenger-$PASSENGER_VERSION/ext/apache2/mod_passenger.so
-   PassengerRoot /usr/local/rvm/gems/$RUBY_VERSION/gems/passenger-$PASSENGER_VERSION
-   PassengerRuby /usr/local/rvm/wrappers/$RUBY_VERSION/ruby
-ENDPASSENGER
-)
-        sudo echo "$PASSENGER" >> /etc/apache2/mods-available/passenger.load
-        sudo a2enmod passenger
-        sudo service apache2 restart
-
+        sudo usermod -a -G rvm vagrant
 fi
 
 
-# Enable the leihs virtual host
-if [ ! -f /etc/apache2/sites-available/leihs ]; then
-VHOST=$(cat <<ENDVHOST
-<VirtualHost *:80>
-   ServerName leihs.local
-   DocumentRoot /vagrant/public/
-
-   <Directory /vagrant/public>
-      AllowOverride all
-      Options -MultiViews
-   </Directory>
-
-</VirtualHost>
-ENDVHOST
-)
-
-        echo "$VHOST" >> /etc/apache2/sites-available/leihs
-        sudo a2ensite leihs
-        sudo service apache2 restart
-fi
-
-
+echo "################## WARNING #######################"
+echo "################## WARNING #######################"
+echo ""
+echo "If this is the first time you use this Vagrant box,"
+echo "you have to log in as user 'vagrant' and run the"
+echo "/vagrant/vagrant-setup.sh script to continue."
+echo ""
+echo "################## WARNING #######################"
+echo "################## WARNING #######################"
---
+++ vagrant-setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# The Ruby version you want to install and use for this Vagrant box
+RUBY_VERSION=ruby-1.9.3-p448
+PASSENGER_VERSION=3.0.9
+
+sudo -i rvm install $RUBY_VERSION
+
+# Install or update Bundler
+rvmsudo gem install bundler
+
+########## Passenger and Apache configuration
+
+# Install Phusion Passenger
+if [ ! -f /etc/apache2/mods-available/passenger.load ]; then
+        rvmsudo gem install passenger -v $PASSENGER_VERSION
+        rvmsudo passenger-install-apache2-module
+
+PASSENGER=$(cat <<ENDPASSENGER
+   LoadModule passenger_module /usr/local/rvm/gems/$RUBY_VERSION/gems/passenger-$PASSENGER_VERSION/ext/apache2/mod_passenger.so
+   PassengerRoot /usr/local/rvm/gems/$RUBY_VERSION/gems/passenger-$PASSENGER_VERSION
+   PassengerRuby /usr/local/rvm/wrappers/$RUBY_VERSION/ruby
+ENDPASSENGER
+)
+        sudo echo "$PASSENGER" >> /etc/apache2/mods-available/passenger.load
+        sudo a2enmod passenger
+        sudo service apache2 restart
+fi
+
+
+# Enable the leihs virtual host
+if [ ! -f /etc/apache2/sites-available/leihs ]; then
+VHOST=$(cat <<ENDVHOST
+<VirtualHost *:80>
+   ServerName leihs.local
+   DocumentRoot /vagrant/public/
+
+   <Directory /vagrant/public>
+      AllowOverride all
+      Options -MultiViews
+   </Directory>
+
+</VirtualHost>
+ENDVHOST
+)
+
+        echo "$VHOST" >> /etc/apache2/sites-available/leihs
+        sudo a2ensite leihs
+        sudo service apache2 restart
+fi
