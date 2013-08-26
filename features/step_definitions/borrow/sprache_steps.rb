58c83d56cc100b41e758ddfad1fc66622406219f
Franco Sellitto <franco.sellitto@zhdk.ch>
2013-08-26 10:53:36 +0200

    [#55072594] test for language switch on borrow section

--- features/step_definitions/borrow/sprache_steps.rb
+++ features/step_definitions/borrow/sprache_steps.rb
@@ -4,6 +4,13 @@ Wenn(/^ich die Sprache auf "(.*?)" umschalte$/) do |language|
   find("a[href*='locale']", :text => language).click
 end
 
-Dann(/^ist die Sprache English$/) do
-  @current_user.reload.language.locale_name.should == "en-GB"
+Dann(/^ist die Sprache "(.*?)"$/) do |language|
+  s = case language
+        when "English"
+          "en-GB"
+        when "Deutsch"
+          "de-CH"
+  end
+  @current_user.reload.language.locale_name.should == s
+  find("a[href=''] strong", :text => language)
 end
