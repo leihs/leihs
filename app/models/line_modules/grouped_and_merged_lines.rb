27731221d7bbe2e0ee74787cbeceb2ded1a63ddd
Franco Sellitto <franco.sellitto@zhdk.ch>
2013-09-02 10:26:31 +0200

    [FIX CI] availability in timeout page and translation

--- app/models/line_modules/grouped_and_merged_lines.rb
+++ app/models/line_modules/grouped_and_merged_lines.rb
@@ -37,7 +37,7 @@ module LineModules
                   start_date: array.first.start_date,
                   end_date: array.first.end_date
               }
-              h[:available?] = array.all? {|l| l.available? } if respond_to? :timeout? and timeout?
+              h[:available?] = array.all? {|l| l.available? } if array.all? {|l| l.class == OrderLine } and array.any? {|l| l.order.timeout? }
               h
             }
           end
