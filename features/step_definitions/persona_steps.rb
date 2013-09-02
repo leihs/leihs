0882af5cc706b034781c9e356d13bac2321e0ceb
Franco Sellitto <franco.sellitto@zhdk.ch>
2013-09-02 13:36:50 +0200

    [FIX CI] minor step change

--- features/step_definitions/persona_steps.rb
+++ features/step_definitions/persona_steps.rb
@@ -8,10 +8,15 @@ Angenommen(/^persona "(.*?)" existing$/) do |persona_name|
   Persona.create persona_name
 end
 
-Given /^I am "([^"]*)"$/ do |persona_name|
+Given /^I am "(.*)"$/ do |persona_name|
+  persona_name = persona_name.gsub "\"", ""
   step 'persona "%s" existing' % persona_name
   step 'man ist eingeloggt als "%s"' % persona_name
   @current_inventory_pool = @current_user.managed_inventory_pools.first 
+end
+
+Angenommen(/^ich bin (.*?)$/) do |persona_name|
+  step 'I am "%s"' % persona_name
 end
 
 Angenommen /^man ist "([^"]*)"$/ do |persona_name|
