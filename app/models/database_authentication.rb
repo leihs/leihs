04166e81efcf0984c23c51bd8293839f08a00441
Franco Sellitto <franco.sellitto@zhdk.ch>
2013-09-05 10:05:52 +0200

    [#55975410] for a user with database authentication, the password is required

--- app/models/database_authentication.rb
+++ app/models/database_authentication.rb
@@ -3,10 +3,9 @@ class DatabaseAuthentication < ActiveRecord::Base
   attr_accessor :password
   
   validates_presence_of     :login
-  validates_presence_of     :password,                   :if => :password_required?
-  validates_presence_of     :password_confirmation,      :if => :password_required?
-  validates_length_of       :password, :within => 4..40, :if => :password_required?
-  validates_confirmation_of :password,                   :if => :password_required?
+  validates_presence_of     :password, :password_confirmation
+  validates_length_of       :password, :within => 4..40
+  validates_confirmation_of :password
   validates_length_of       :login,    :within => 3..40
   validates_uniqueness_of   :login, :case_sensitive => false
 
@@ -20,29 +19,20 @@ class DatabaseAuthentication < ActiveRecord::Base
     u && u.authenticated?(password) ? u : nil
   end
 
-  # Encrypts some data with the salt.
-  def self.encrypt(password, salt)
-    Digest::SHA1.hexdigest("--#{salt}--#{password}--")
-  end
-
-  # Encrypts the password with the user salt
-  def encrypt(password)
-    self.class.encrypt(password, salt)
-  end
-
   def authenticated?(password)
     crypted_password == encrypt(password)
   end
 
+  private
+
+  def encrypt(password)
+    Digest::SHA1.hexdigest("--#{salt}--#{password}--")
+  end
+
   def encrypt_password
-    return if password.blank?
+    return if password == "_password_"
     self.salt = Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{login}--") if new_record?
     self.crypted_password = encrypt(password)
   end
-    
-  def password_required?
-    crypted_password.blank? || !password.blank?
-  end
 
 end
-
