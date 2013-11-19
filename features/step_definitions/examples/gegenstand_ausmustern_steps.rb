fcb14bcea64fc011fc2e5ed90c6518341cf80596
Sebastian Pape <github@pape.io>
2013-11-19 08:56:10 +0100

    [FIX] gegenstand_ausmuster2.feature

--- features/step_definitions/examples/gegenstand_ausmustern_steps.rb
+++ features/step_definitions/examples/gegenstand_ausmustern_steps.rb
@@ -76,6 +76,7 @@ end
 
 Dann(/^dieses Gegenstand ist nicht mehr ausgemustert$/) do
   @item.reload.should_not be_retired
+  sleep(0.88) # fix lazy request problem
 end
 
 Wenn(/^die Anschaffungskategorie ist ausgewählt$/) do
