31096e39f63cd5f2f3b045ca990fc5c7a5ee5c75
Sebastian Pape <github@pape.io>
2013-11-28 12:08:50 +0100

    [#61470186 #61468436] show correct time range for orders on daily view

--- features/step_definitions/examples/tagesansicht_steps.rb
+++ features/step_definitions/examples/tagesansicht_steps.rb
@@ -2,11 +2,14 @@
 
 Angenommen(/^eine Bestellungen mit zwei unterschiedlichen Zeitspannen existiert$/) do
   @customer = @current_inventory_pool.users.customers.sample
-  @contract = FactoryGirl.create :contract, user_id: @customer.id, status: 'approved'
+  @contract = FactoryGirl.create :contract, user_id: @customer.id, status: 'submitted', inventory_pool_id: @current_inventory_pool.id
   FactoryGirl.create :contract_line, contract_id: @contract.id, start_date: Date.today, end_date: Date.tomorrow 
   FactoryGirl.create :contract_line, contract_id: @contract.id, start_date: Date.today, end_date: Date.today+10.days 
 end
 
 Dann(/^sehe ich für diese Bestellung die längste Zeitspanne direkt auf der Linie$/) do
-  pending
+  visit manage_daily_view_path(@current_inventory_pool)
+  line_with_max_range = @contract.item_lines.max{|line| line.end_date - line.start_date}
+  range = (line_with_max_range.end_date-line_with_max_range.start_date)+1
+  find(".line[data-id='#{@contract.id}']").should have_content "#{range} #{_('days')}"
 end
