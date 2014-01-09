c0360da1b6ca2a25b3aa45e82aaf19c2356886c7
Ramón Cahenzli <rca@psy-q.ch>
2014-01-09 17:15:16 +0100

    Importer for Mike's new CSV format.

--- doc/other/csv_import_of_items.rb
+++ doc/other/csv_import_of_items.rb
@@ -1,68 +1,14 @@
+# encoding: utf-8
 # This is to be run from the Rails console using require:
-# ./script/console
-# require 'doc/csv_import_of_items'
-
-
-
+# rails c
+# require 'doc/other/csv_import_of_items'
 
 import_file = "/tmp/items.csv"
 
 @failures = 0
 @successes = 0
 
-def create_item(model_name, inventory_code, serial_number, manufacturer, category, accessory_string, note)
-  
-  m = Model.find_by_name(model_name)
-  if m.nil?
-    m = create_model(model_name, category, manufacturer, accessory_string)    
-  end
-  
-  ip = InventoryPool.find_by_name("MMP")
-  
-  i = Item.new
-  i.model = m
-  i.inventory_code = inventory_code
-  i.serial_number = serial_number
-  i.note = note
-  i.owner = ip
-  i.is_borrowable = true
-  i.is_inventory_relevant = true
-  i.inventory_pool = ip
-  
-  if i.save
-    puts "Item imported correctly:"
-    @successes += 1
-    puts i.inspect
-  else
-    @failures += 1
-    puts "Could not import item #{inventory_code}"
-  end
-  
-  puts "-----------------------------------------"
-  puts "DONE"
-  puts "#{@successes} successes, #{@failures} failures"
-  puts "-----------------------------------------"
-  
-end
-
-def create_model(name, category, manufacturer, accessory_string)
-  c = Category.find_or_create_by_name(category)
-  m = Model.create(:name => name, :manufacturer => manufacturer)
-  m.categories << c
-  
-  unless accessory_string.blank?  
-    accessory_string.split(",").each do |string|
-      acc = Accessory.create(:name => string.strip)
-      m.accessories << acc
-    end
-  end
-  
-  m.save
-
-  return m
-end
-
-items_to_import = CSV.open(import_file, :headers => true)
+items_to_import = CSV.open(import_file, :col_sep => "\t", :headers => true)
 
 # CSV fields:
 # 0: Bezeichnung
@@ -74,14 +20,66 @@ items_to_import = CSV.open(import_file, :headers => true)
 # 6: Defekt: (-> Notiz)
 
 items_to_import.each do |item|
-  create_item(item["Bezeichnung"], 
-              item["Inventarnummer"],
-              item["Seriennummer"],
-              item["Hersteller"],
-              item["Typ"],
-              item["Zubehör"],
-              item["Defekt:"])
+  i = Item.new
+  i.model = Model.find(item["Leihs - Modellnummer"])
+  i.inventory_code = item["Inv-Code:"]
+  i.serial_number = item["Seriennummer"]
+  i.note = item["Notitz"] # That typo is in the original CSV file already
+
+  # Inventory relevance
+  i.is_inventory_relevant = false
+  i.is_inventory_relevant = true if item["Inventarrelevant:"] == "JA"
+
+  # Borrowability
+  i.is_borrowable = false
+
+  # Ownership
+  owner_ip = InventoryPool.where(:name => item["Besitzer"]).first
+  i.owner = owner_ip
+
+  # Responsible department
+  responsible_ip = InventoryPool.where(:name => item["Verantwortliche Abteilung"]).first
+  i.inventory_pool = responsible_ip
+
+  # Building and room
+  building_code = item["Gebäude"].match(/.*\((.*)\)$/)[1]
+  b = Building.where(:code => building_code).first
+  room = nil
+  room = item["Raum"] unless item["Raum"].blank?
+  i.location = Location.find_or_create({:building_id => b.code, :room => room})
+
+  # Invoice
+  i.invoice_number = item["Rechungsnummer"]
+  i.invoice_date = Date.strptime(item["Rechnungsdatum"], "%m/%d/%Y") unless item["Rechnungsdatum"].blank?
+
+  i.last_check = Date.strptime(item["letzte Inventur"], "%m/%d/%Y") unless item["letzte Inventur"].blank?
+
+  # Supplier
+  i.supplier = Supplier.where(:name => item["Lieferant"]).first
+  if i.supplier.nil?
+    i.supplier = Supplier.create(:name => item["Lieferant"])
+  end
+
+  # Properties
+  i.properties[:anschaffungskategorie] = item["Anschaffungskategorie"]
+  i.properties[:reference] = "invoice" if item["Bezug"] == "laufende Rechnung"
+  i.properties[:reference] = "investment" if item["Bezug"] == "Investition"
+
+  puts i
+
+
+  #if i.save
+  #  puts "Item imported correctly:"
+  #  @successes += 1
+  #  puts i.inspect
+  #else
+  #  @failures += 1
+  #  puts "Could not import item #{inventory_code}"
+  #end
+
 end
 
-
-
+puts "-----------------------------------------"
+puts "DONE"
+puts "#{@successes} successes, #{@failures} failures"
+puts "-----------------------------------------"
