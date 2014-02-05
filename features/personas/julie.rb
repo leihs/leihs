8eb442f2d2790a749dce327a38edfc9c714ad3f6
Matus Kmit <matus.kmit@zhdk.ch>
2014-02-05 16:44:01 +0100

    extend julie persona WIP

--- features/personas/julie.rb
+++ features/personas/julie.rb
@@ -19,13 +19,13 @@ module Persona
       ActiveRecord::Base.transaction do
         select_inventory_pool
         create_user
-        create_delegation
-        create_contracts
+        create_delegations
+        create_submitted_contracts
       end
     end
 
     def setup_dependencies 
-      Persona.create :mina
+      @mina = Persona.create :mina
     end
 
     def select_inventory_pool
@@ -33,38 +33,86 @@ module Persona
     end
 
     def create_user
-      @user = FactoryGirl.create(:user, firstname: @@name, lastname: @@lastname, login: @@name.downcase, email: @@email)
-      @user.access_rights.create(:role => :customer, :inventory_pool => @inventory_pool)
+      @julie = FactoryGirl.create(:user, firstname: @@name, lastname: @@lastname, login: @@name.downcase, email: @@email)
+      @julie.access_rights.create(:role => :customer, :inventory_pool => @inventory_pool)
     end
 
-    def create_delegation
-      @delegation = FactoryGirl.create(:user,
-                                       delegator_user: @user,
-                                       firstname: Faker::Lorem.sentence,
-                                       lastname: nil,
-                                       login: nil,
-                                       phone: nil,
-                                       authentication_system: nil,
-                                       unique_id: nil,
-                                       email: nil,
-                                       badge_id: nil,
-                                       address: nil,
-                                       city: nil,
-                                       country: nil,
-                                       zip: nil,
-                                       language: nil)
-      @delegation.users << Persona.get(:mina)
-      @delegation.access_rights.create(:role => :customer, :inventory_pool => @inventory_pool)
+    def create_delegations
+      @delegation1 = FactoryGirl.create(:user,
+                                        delegator_user: @julie,
+                                        firstname: Faker::Lorem.sentence,
+                                        lastname: nil,
+                                        login: nil,
+                                        phone: nil,
+                                        authentication_system: nil,
+                                        unique_id: nil,
+                                        email: nil,
+                                        badge_id: nil,
+                                        address: nil,
+                                        city: nil,
+                                        country: nil,
+                                        zip: nil,
+                                        language: nil)
+      @delegation1.users << @mina
+      @delegation1.access_rights.create(:role => :customer, :inventory_pool => @inventory_pool)
+
+      @delegation2 = FactoryGirl.create(:user,
+                                        delegator_user: @mina,
+                                        firstname: Faker::Lorem.sentence,
+                                        lastname: nil,
+                                        login: nil,
+                                        phone: nil,
+                                        authentication_system: nil,
+                                        unique_id: nil,
+                                        email: nil,
+                                        badge_id: nil,
+                                        address: nil,
+                                        city: nil,
+                                        country: nil,
+                                        zip: nil,
+                                        language: nil)
+      @delegation2.users << @julie
+      @delegation2.access_rights.create(:role => :customer, :inventory_pool => @inventory_pool)
+
+      # delegation without access rights
+      @delegation3 = FactoryGirl.create(:user,
+                                        delegator_user: @mina,
+                                        firstname: Faker::Lorem.sentence,
+                                        lastname: nil,
+                                        login: nil,
+                                        phone: nil,
+                                        authentication_system: nil,
+                                        unique_id: nil,
+                                        email: nil,
+                                        badge_id: nil,
+                                        address: nil,
+                                        city: nil,
+                                        country: nil,
+                                        zip: nil,
+                                        language: nil)
+      @delegation3.users << @julie
     end
 
-    def create_contracts
-      contract = FactoryGirl.create(:contract, :user => @delegation, :delegated_user => @user, :inventory_pool => @inventory_pool, :status => :submitted)
+    def create_submitted_contracts
+      contract = FactoryGirl.create(:contract, :user => @delegation1, :delegated_user => @julie, :inventory_pool => @inventory_pool, :status => :submitted)
       contract_purpose = FactoryGirl.create :purpose, :description => Faker::Lorem.sentence
 
       rand(3..5).times do
         item = FactoryGirl.create :item, inventory_pool: @inventory_pool
         contract.contract_lines << FactoryGirl.create(:contract_line, :purpose => contract_purpose, :contract => contract, :model => item.model)
       end
+    end
+
+    def create_approved_contracts
+      contract = FactoryGirl.create(:contract, :user => @delegation1, :delegated_user => @julie, :inventory_pool => @inventory_pool, :status => :submitted)
+      contract_purpose = FactoryGirl.create :purpose, :description => Faker::Lorem.sentence
+
+      rand(3..5).times do
+        item = FactoryGirl.create :item, inventory_pool: @inventory_pool
+        contract.contract_lines << FactoryGirl.create(:contract_line, :purpose => contract_purpose, :contract => contract, :model => item.model)
+      end
+
+      contract.sign Persona.get(:pius)
     end
 
   end
