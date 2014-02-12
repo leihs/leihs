28510992abaea2beeebf3d84966fbe18164af383
Franco Sellitto <franco.sellitto@zhdk.ch>
2014-02-12 14:28:57 +0100

    adapting cucumber env

--- features/support/hover_for_tooltip.rb
+++
@@ -1,8 +0,0 @@
-def hover_for_tooltip(target)
-  sleep(0.99) # wait for potential previous tooltips
-  find("footer").click # move mouse somewhere else to ensure its currently not over the target
-  target.click
-  target.click
-  sleep(0.99) # wait for the css transition
-  all(".tooltipster-content") # there should be just one
-end
