c5958859e9c873bc1132b54360027dec50848d09
Ramón Cahenzli <rca@psy-q.ch>
2014-03-25 14:47:40 +0100

    Proper Ruby version strings.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_type, :system
 set :rvm_path, "/usr/local/rvm" 
-set :rvm_ruby_string, '1.9.3'        # Or whatever env you want it to run in.
+set :rvm_ruby_string, '2.1.0'        # Or whatever env you want it to run in.
 
 
 require "bundler/capistrano"
