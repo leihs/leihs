97b7697a20126d993bc50ac7c649d8c20205e362
Matus Kmit <matus.kmit@zhdk.ch>
2014-04-08 13:33:46 +0200

    [DOMINA] add space in commands

--- domina/lib/domina/database.rb
+++ domina/lib/domina/database.rb
@@ -5,12 +5,12 @@ module Domina
     class << self
 
       def create_db config
-        puts Domina::System.execute_cmd! "mysql -u#{config['username']} --password=#{config['password']} -e 'drop database if exists #{config['database']}'"
-        puts Domina::System.execute_cmd! "mysql -u#{config['username']} --password=#{config['password']} -e 'create database if not exists #{config['database']}'"
+        puts Domina::System.execute_cmd! "mysql -u #{config['username']} --password=#{config['password']} -e 'drop database if exists #{config['database']}'"
+        puts Domina::System.execute_cmd! "mysql -u #{config['username']} --password=#{config['password']} -e 'create database if not exists #{config['database']}'"
       end
 
       def drop_db config
-        puts Domina::System.execute_cmd! "mysql -u#{config['username']} --password=#{config['password']} -e 'drop database if exists #{config['database']}'"
+        puts Domina::System.execute_cmd! "mysql -u #{config['username']} --password=#{config['password']} -e 'drop database if exists #{config['database']}'"
       end
 
     end
