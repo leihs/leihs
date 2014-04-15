dcb30bb67b42f129996c62db6d0f205292c1f98b
Ramón Cahenzli <rca@psy-q.ch>
2014-04-15 13:16:00 +0200

    Fixing LDAP controller spec.

--- spec/controllers/ldap_controller_spec.rb
+++ spec/controllers/ldap_controller_spec.rb
@@ -39,7 +39,7 @@ describe Authenticator::LdapAuthenticationController do
 
   context "if the user does not yet exist" do
     it "should be able to create a normal with various useful data grabbed from LDAP" do
-      post :login, {:login => { :username => "normal_user", :password => "pass" }}, {}
+      post :login, {:login => { :user => "normal_user", :password => "pass" }}, {}
       User.where(:login => "normal_user").first.should_not == nil
       # TODO: Check that all the data from LDAP made it into our user object
     end
@@ -51,7 +51,7 @@ describe Authenticator::LdapAuthenticationController do
       ips_with_automatic_access = InventoryPool.all.sample(2)
       ips_with_automatic_access.each {|ip| ip.update_attributes automatic_access: true}
 
-      post :login, {:login => { :username => "normal_user", :password => "pass" }}, {}
+      post :login, {:login => { :user => "normal_user", :password => "pass" }}, {}
 
       user = User.where(:login => "normal_user").first
       access_rights = user.access_rights.where(role: "customer")
@@ -62,7 +62,7 @@ describe Authenticator::LdapAuthenticationController do
 
   context "if the user is in the admin DN on LDAP" do
     it "should give that user the admin role" do
-      post :login, {:login => { :username => "admin_user", :password => "pass" }}, {}
+      post :login, {:login => { :user => "admin_user", :password => "pass" }}, {}
       user = User.where(:login => "admin_user").first
       user.access_rights.active.collect(&:role).include?(:admin).should == true
     end
@@ -70,7 +70,7 @@ describe Authenticator::LdapAuthenticationController do
 
   context "if the user is not in the admin DN on LDAP" do
     it "should not give that user the admin role" do
-      post :login, {:login => { :username => "normal_user", :password => "pass" }}, {}
+      post :login, {:login => { :user => "normal_user", :password => "pass" }}, {}
       user = User.where(:login => "normal_user").first
       user.access_rights.active.collect(&:role).include?(:admin).should == false
     end
