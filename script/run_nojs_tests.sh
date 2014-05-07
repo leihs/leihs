e1cd8cdf39264405310c6e01e3cdf57aa2e16f38
Ramón Cahenzli <rca@psy-q.ch>
2014-05-07 10:44:35 +0200

    Tweak for better output.

--- script/run_nojs_tests.sh
+++ script/run_nojs_tests.sh
@@ -8,5 +8,6 @@ bundle exec rspec spec/
 bundle exec cucumber -t ~@javascript features/
 
 if [ -f tmp/rerun.txt ]; then
+  echo "Rerun necessary, the first execution exited with non-0."
   bundle exec cucumber -t ~@javascript -p rerun
 fi
