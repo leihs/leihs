ff2ac669fe017f78aaaae64fe7a48b74f75a2a14
Ramón Cahenzli <rca@psy-q.ch>
2014-05-07 15:21:50 +0200

    Fix #46: Create default admin user without using Faker or FactoryGirl.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,28 @@
 require "#{Rails.root}/features/support/leihs_factory.rb"
-LeihsFactory.create_minimal_setup
+
+LeihsFactory.create_default_languages
+LeihsFactory.create_default_authentication_systems
+LeihsFactory.create_default_building
+
+admin = User.where(:login => 'admin').first
+unless admin
+  admin = User.create(:email => "admin@example.com",
+                      :login => "admin",
+                      :language_id => Language.default_language.id,
+                      :firstname => "Admin",
+                      :lastname => "Admin")
+
+  admin.save
+  admin.access_rights.create(:role => :admin)
+
+  dba = DatabaseAuthentication.create(:login => "admin",
+                                      :password => "password",
+                                      :password_confirmation => "password")
+  dba.user = admin
+  if dba.save
+    puts "The administrator user 'admin' has been created with password 'password'"
+  else
+    puts "There was an error creating the login credentials for the default admin user 'admin'. The error was: #{dba.errors.full_messages}"
+  end
+end
+
