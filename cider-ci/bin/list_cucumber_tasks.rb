3a1c45b4f294054326ffe8339676d7f118455263
Franco Sellitto <franco.sellitto@zhdk.ch>
2014-05-09 12:17:31 +0200

    create base and strict execution definitions for Cider-CI

--- cider-ci/bin/list_cucumber_tasks.rb
+++ cider-ci/bin/list_cucumber_tasks.rb
@@ -2,18 +2,30 @@
 require 'yaml'
 require 'pry'
 
-def task_for_feature_file file_path
+def task_for_feature_file file_path, timeout = 200, strict = false
   name= file_path.match(/features\/(.*)\.feature/).captures.first
-  exec = %{bundle exec cucumber -p default --strict "#{file_path}"}
-  {"name" => name,
+  exec = %{bundle exec cucumber -p default #{strict ? "--strict " : nil}"#{file_path}"}
+  { "name" => name,
     "scripts" => {
-    "cucumber" => {
-        "timeout" => 600,
-        "body" => exec }
-    }
+      "cucumber" => {
+          "timeout" => timeout,
+          "body" => exec }
+      }
   }
 end
 
-STDOUT.write \
-  Dir.glob("features/**/*.feature") \
-  .map{|f| task_for_feature_file(f)}.to_yaml
+feature_files = Dir.glob("features/**/*.feature")
+
+filepath = "./cider-ci/tasks/cucumber.yml"
+File.open(filepath,"w") do |f|
+  f.write(feature_files.map do |f|
+    task_for_feature_file(f, 600)
+  end.to_yaml)
+end
+
+filepath = "./cider-ci/tasks/cucumber_strict.yml"
+File.open(filepath,"w") do |f|
+  f.write(feature_files.map do |f|
+    task_for_feature_file(f, 600, true)
+  end.to_yaml)
+end
