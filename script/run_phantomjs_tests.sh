2453f32026c9c84aad6c5e3e35f6f2e2005f3507
Ramón Cahenzli <rca@psy-q.ch>
2014-05-13 10:00:01 +0200

    Small script to run just PhantomJS tests.

---
+++ script/run_phantomjs_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bash script/validate_gettext_files.sh
+
+bundle exec rake leihs:reset
+bundle exec rake app:test:generate_personas_dumps
+bundle exec rspec spec/
+bundle exec cucumber -t @javascript -t @phantomjs features/
+
+if [ -f tmp/rerun.txt ]; then
+  echo "Rerun necessary, the first execution exited with non-0."
+  bundle exec cucumber -t @javascript -t @phantomjs -p rerun
+fi
