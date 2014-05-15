e3f348dc8316900f635cbc93c00464f8059d6668
Ramón Cahenzli <rca@psy-q.ch>
2014-05-15 17:38:48 +0200

    Refining and handling some broken migrations.

--- script/validate_migrations.rb
+++ script/validate_migrations.rb
@@ -1,68 +1,20 @@
+# encoding: utf-8
 require 'rubygems'
 require 'pry'
+require "./#{File.join(File.dirname(__FILE__), "lib", "semverly")}"
 
 REPO_URL = "https://github.com/zhdk/leihs.git"
 TARGET_DIR = File.join("/tmp", "migrations")
+
 # If no :ruby_version is given, we use this
 DEFAULT_RUBY_VERSION = '2.1.1'
 
-SUPPORTED_MIGRATIONS = [
-
-                         { :ruby_version => '1.9.3-p545',
-                           :from => '2.9.14',
-                           :to => '3.0.1' },
-
-                         { :ruby_version => '1.9.3-p545',
-                           :from => '3.0.1',
-                           :to => '3.0.2' },
-
-                         { :from => '3.0.2',
-                           :to => '3.0.3' },
-
-                         { :from => '3.0.3',
-                           :to => '3.0.4' },
-
-                         { :from => '3.0.4',
-                           :to => '3.1.0' },
-
-                         { :from => '3.1.0',
-                           :to => '3.2.0' },
-
-                         { :from => '3.2.0',
-                           :to => '3.2.1' },
-
-                         { :from => '3.2.1',
-                           :to => '3.3.0' },
-
-                         { :from => '3.3.0',
-                           :to => '3.3.1' },
-
-                         { :from => '3.3.1',
-                           :to => '3.3.2' },
-
-                         { :from => '3.3.2',
-                           :to => '3.4.0' },
-
-                         { :from => '3.4.0',
-                           :to => '3.5.0' },
-
-                         { :from => '3.5.0',
-                           :to => '3.5.1' },
-
-                         { :from => '3.5.1',
-                           :to => '3.6.0' },
-
-                         { :from => '3.6.0',
-                           :to => '3.6.1' }
-]
-
-
-def write_database_config(target_path)
-
+def write_database_config(target_path, mysql_version = "mysql2")
+  mysql_version ||= "mysql2"
   database_config = { "production" => { "host" => "localhost",
                                         "username" => "jenkins",
                                         "password" => "jenkins",
-                                        "adapter" => "mysql2",
+                                        "adapter" => mysql_version,
                                         "database" => "leihs_migration_tests" } }
 
   config_file = File.open(target_path, "w+")
@@ -97,16 +49,18 @@ def wrap(command, ruby_version)
 end
 
 def switch_to_tag(tag)
-  Dir.chdir(TARGET_DIR) do
-    changed = `git status | egrep "(database.yml|schema.rb)"`
-    if changed != ""
-      system("git checkout -- config/database.yml") if File.exist?(File.join(".", "config", "database.yml"))
-      system("git checkout -- db/schema.rb") if File.exist?(File.join(".", "db", "schema.rb"))
-    end
-    system("git checkout #{tag}")
-    database_config_file_path = File.join(".", "config", "database.yml")
-    write_database_config(database_config_file_path)
+  Dir.chdir(TARGET_DIR)
+  changed = `git status | egrep "(database.yml|schema.rb)"`
+  if changed != ""
+    system("git checkout -- config/database.yml") if File.exist?(File.join(".", "config", "database.yml"))
+    system("git checkout -- db/schema.rb") if File.exist?(File.join(".", "db", "schema.rb"))
   end
+  system("git checkout #{tag}")
+  mysql2 = `grep mysql2 Gemfile`
+  mysql_version = "mysql" if mysql2 == ""
+
+  database_config_file_path = File.join(".", "config", "database.yml")
+  write_database_config(database_config_file_path, mysql_version)
 end
 
 def attempt_migration(ruby_version: DEFAULT_RUBY_VERSION, from: nil, to: nil)
@@ -115,16 +69,15 @@ def attempt_migration(ruby_version: DEFAULT_RUBY_VERSION, from: nil, to: nil)
     raise "Need to give both a from and a to version"
   end
 
-  Dir.chdir(TARGET_DIR) do
-    puts "Trying migrations inside #{TARGET_DIR}"
-    switch_to_tag(from)
-    system(wrap("bundle install --deployment --without test development --path=#{TARGET_DIR}/bundle", ruby_version))
-    system(wrap("bundle exec rake db:drop db:create db:migrate", ruby_version ))
+  Dir.chdir(TARGET_DIR)
+  puts "Trying migrations inside #{TARGET_DIR}"
+  switch_to_tag(from)
+  system(wrap("bundle install --deployment --without test development cucumber --path=#{TARGET_DIR}/bundle", ruby_version))
+  system(wrap("bundle exec rake db:drop db:create db:migrate", ruby_version )) or return false
 
-    switch_to_tag(to)
-    system(wrap("bundle install --deployment --without test development --path=#{TARGET_DIR}/bundle", ruby_version))
-    output = `#{system(wrap("bundle exec rake db:migrate", ruby_version))}`
-  end
+  switch_to_tag(to)
+  system(wrap("bundle install --deployment --without test development cucumber --path=#{TARGET_DIR}/bundle", ruby_version))
+  output = `#{system(wrap("bundle exec rake db:migrate", ruby_version))}`
 
   if $?.exitstatus == 0
     return true
@@ -150,21 +103,76 @@ def setup_target_directory
 end
 
 
+def get_versions(higher_than = nil)
+  Dir.chdir(TARGET_DIR)
+  output = `git tag --list`
+  if higher_than
+    versions = output.split("\n").map { |s| 
+      SemVer.parse(s) 
+    }.compact.sort.select{ |v| 
+      v > SemVer.parse(higher_than)
+    }.map(&:to_s)
+  else
+    versions = output.split("\n").map { |s| SemVer.parse(s) }.compact.sort.map(&:to_s)
+  end
+
+  # Filter out alphas and betas and rcs
+  versions = versions.select{ |v|
+    v if v.match(/(beta|alpha|rc)/).nil?
+  }
+  return versions
+end
+
+
+def lookup_ruby_versions_for(version)
+  version_map = {
+    "2.9.13" => ["1.8.7-p375"],
+    "2.9.14" => ["1.8.7-p375"],
+    "3.0.0" => ["1.9.3-p545", "2.1.1"],
+    "3.0.1" => ["1.9.3-p545", "2.1.1"],
+    "3.0.2" => ["1.9.3-p545", "2.1.1"],
+    "3.0.3" => ["1.9.3-p545", "2.1.1"],
+    "3.0.4" => ["1.9.3-p545", "2.1.1"],
+    "3.1.0" => ["1.9.3-p545", "2.1.1"],
+    "3.2.0" => ["1.9.3-p545", "2.1.1"],
+    "3.3.0" => ["1.9.3-p545", "2.1.1"],
+    "3.3.1" => ["1.9.3-p545", "2.1.1"],
+    "3.3.2" => ["1.9.3-p545", "2.1.1"],
+    "3.4.0" => ["1.9.3-p545", "2.1.1"]
+  }
+
+  if version_map[version]
+    return version_map[version]
+  else
+    return [DEFAULT_RUBY_VERSION]
+  end
+end
+
 def attempt_migrations
   error_messages = []
   error_count = 0
 
-  SUPPORTED_MIGRATIONS.each do |mig|
-    ruby_version = DEFAULT_RUBY_VERSION
-    ruby_version = mig[:ruby_version] if mig[:ruby_version]
+  versions = get_versions("2.9.12")
 
-    if attempt_migration(:from => mig[:from], :to => mig[:to], :ruby_version => ruby_version) == true
-      puts "Migration from #{mig[:from]} to #{mig[:to]} using Ruby #{ruby_version} was successful."
-    else
-      error_message = "Migration from #{mig[:from]} to #{mig[:to]} using Ruby #{ruby_version} has failed."
-      error_messages << error_message
-      puts error_message
-      error_count += 1
+  versions.each do |version|
+    # Get all versions higher than the current one
+    target_versions = get_versions(version)
+    puts ["---------- this is the big one, oh my ---------------",
+          "Will try to migrate from #{version} to #{target_versions.join(", ")}",
+          "-----------------------------------------------------"]
+    target_versions.each do |target_version|
+      ruby_versions = lookup_ruby_versions_for(target_version)
+      ruby_versions.each do |ruby_version|
+        puts "Attempting migration from #{version} to #{target_version} using Ruby #{ruby_version}."
+        if attempt_migration(:from => version, :to => target_version, :ruby_version => ruby_version) == true
+          puts "Migration from #{version} to #{target_version} using Ruby #{ruby_version} was successful."
+        else
+          error_message = "Migration from #{version} to #{target_version} using Ruby #{ruby_version} has failed."
+          error_messages << error_message
+          puts error_message
+          error_count += 1
+        end
+      end
     end
   end
 
