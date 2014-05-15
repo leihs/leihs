70300e5d03986c52e0f5b0489441f20e3ad3f549
Ramón Cahenzli <rca@psy-q.ch>
2014-05-15 18:45:37 +0200

    Validation of migrations.
    Squashed commit of the following:
    
    commit 5b8fc6528b66fb1a0e9b928c48bec7163bcd21e6
    Merge: f83f773 e2e8ec3
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 18:45:05 2014 +0200
    
        Merge branch 'next' into rca_validate_migrations
    
    commit f83f773961abbb35faf51e60eece31cf6725accd
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 18:25:26 2014 +0200
    
        One more supported version.
    
    commit 14bc5476d5ef24dee288ce163bcce87382c3b88f
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 18:02:34 2014 +0200
    
        Better logging.
    
    commit 804d2318cc0ebd07b0925fd39d52f5010df224cc
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 17:46:09 2014 +0200
    
        Skip combinations that won't work.
    
    commit e3f348dc8316900f635cbc93c00464f8059d6668
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 17:38:48 2014 +0200
    
        Refining and handling some broken migrations.
    
    commit 1128ff72ad2704016cf237ca32af9a317f160c38
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 15:20:44 2014 +0200
    
        More elegant checking.
    
    commit d18cc997aea2c07dac4429e5de457c81cbf675f6
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 15:18:19 2014 +0200
    
        Wrong sequence of things, and mysql (1) support no longer necessary.
    
    commit 6c7cd8340084c970f0181f1c3c2f35ba3018cfab
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 15:10:55 2014 +0200
    
        Fixed checking out different versions.
    
    commit 0b20ee67b10c828262a2f74219d3e2ba1b10fd87
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 15:03:55 2014 +0200
    
        Add all our supported versions.
    
    commit 178c84a647dd69462041b3947cd55580f6fb2ffb
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 14:45:16 2014 +0200
    
        First real working version.
    
    commit 59cf6c469148a118f0feadb2cf6a1492ba0ebdf8
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 12:12:36 2014 +0200
    
        Now we're getting somewhere.
    
    commit 73b67c085427c7f07f5c7fdfa6b08eadd0261571
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu May 15 10:44:33 2014 +0200
    
        More attempts that don't work.
    
    commit 09d12c5a03bf4d7b3b9a1dfb659cbaee010e2a60
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Wed May 14 18:12:23 2014 +0200
    
        Work in progress on validating migrations.

---
+++ script/validate_migrations.rb
@@ -0,0 +1,209 @@
+# encoding: utf-8
+require 'rubygems'
+require 'pry'
+require 'logger'
+require "./#{File.join(File.dirname(__FILE__), "lib", "semverly")}"
+
+REPO_URL = "https://github.com/zhdk/leihs.git"
+TARGET_DIR = File.join("/tmp", "migrations")
+
+# If no :ruby_version is given, we use this
+DEFAULT_RUBY_VERSION = '2.1.1'
+
+$logger = Logger.new(File.join("log", "validate_migrations.log"))
+$logger.level = Logger::INFO
+
+def write_database_config(target_path, mysql_version = "mysql2")
+  mysql_version ||= "mysql2"
+  database_config = { "production" => { "host" => "localhost",
+                                        "username" => "jenkins",
+                                        "password" => "jenkins",
+                                        "adapter" => mysql_version,
+                                        "database" => "leihs_migration_tests" } }
+
+  config_file = File.open(target_path, "w+")
+  config_file.puts database_config.to_yaml
+  config_file.close
+end
+
+
+def check_and_install_ruby(ruby_version)
+  if system("bash -l -c 'rbenv version #{ruby_version}'") == false
+    system("bash -l -c 'rbenv install #{ruby_version}'")
+    system("bash -l -c 'rbenv shell #{ruby_version} && gem install bundler'")
+  else
+    $logger.debug "Ruby #{ruby_version} was already installed, skipping installation."
+    if system("bash -l -c 'rbenv shell #{ruby_version} && bundle --version'") == false
+       $logger.debug "Installing bundler for #{ruby_version}"
+      system("bash -l -c 'rbenv shell #{ruby_version} && gem install bundler'")
+    else
+      $logger.debug "Ruby #{ruby_version} already has Bundler, skipping that."
+    end
+  end
+end
+
+def wrap(command, ruby_version)
+  check_and_install_ruby(ruby_version)
+  prefix = "bash -l -c 'rbenv shell #{ruby_version} && cd #{TARGET_DIR} && export RAILS_ENV=production && "
+  postfix = "'"
+  command = "#{prefix}#{command}#{postfix}"
+  return command
+end
+
+def switch_to_tag(tag)
+  Dir.chdir(TARGET_DIR)
+  changed = `git status | egrep "(database.yml|schema.rb)"`
+  if changed != ""
+    system("git checkout -- config/database.yml") if File.exist?(File.join(".", "config", "database.yml"))
+    system("git checkout -- db/schema.rb") if File.exist?(File.join(".", "db", "schema.rb"))
+  end
+  system("git checkout #{tag}")
+  mysql2 = `grep mysql2 Gemfile`
+  mysql_version = "mysql" if mysql2 == ""
+
+  database_config_file_path = File.join(".", "config", "database.yml")
+  write_database_config(database_config_file_path, mysql_version)
+end
+
+def attempt_migration(ruby_version: DEFAULT_RUBY_VERSION, from: nil, to: nil)
+
+  if (from == nil or to == nil)
+    raise "Need to give both a from and a to version"
+  end
+
+  Dir.chdir(TARGET_DIR)
+  $logger.debug "Trying migrations inside #{TARGET_DIR}"
+  switch_to_tag(from)
+  system(wrap("bundle install --deployment --without test development cucumber --path=#{TARGET_DIR}/bundle", ruby_version))
+  system(wrap("bundle exec rake db:drop db:create db:migrate", ruby_version )) or return false
+
+  switch_to_tag(to)
+  system(wrap("bundle install --deployment --without test development cucumber --path=#{TARGET_DIR}/bundle", ruby_version))
+  output = `#{system(wrap("bundle exec rake db:migrate", ruby_version))}`
+
+  if $?.exitstatus == 0
+    return true
+  else
+    $logger.error "Error during migration attempt to #{to}: #{output}"
+    return false
+  end
+end
+
+def setup_target_directory
+  if File.exist?(TARGET_DIR)
+    Dir.chdir(TARGET_DIR)
+    git = system("git status")
+    if git
+      system("git fetch")
+    else
+      system("rm -rf #{TARGET_DIR}")
+      system("git clone #{REPO_URL} #{TARGET_DIR}")
+    end
+  else
+    system("git clone #{REPO_URL} #{TARGET_DIR}")
+  end
+end
+
+
+def get_versions(higher_than = nil)
+  Dir.chdir(TARGET_DIR)
+  output = `git tag --list`
+  if higher_than
+    versions = output.split("\n").map { |s| 
+      SemVer.parse(s) 
+    }.compact.sort.select{ |v| 
+      v > SemVer.parse(higher_than)
+    }.map(&:to_s)
+  else
+    versions = output.split("\n").map { |s| SemVer.parse(s) }.compact.sort.map(&:to_s)
+  end
+
+  # Filter out alphas and betas and rcs
+  versions = versions.select{ |v|
+    v if v.match(/(beta|alpha|rc)/).nil?
+  }
+  return versions
+end
+
+
+def lookup_ruby_versions_for(version)
+  version_map = {
+    "2.9.13" => ["1.8.7-p375"],
+    "2.9.14" => ["1.8.7-p375"],
+    "3.0.0" => ["1.9.3-p545", "2.1.1"],
+    "3.0.1" => ["1.9.3-p545", "2.1.1"],
+    "3.0.2" => ["1.9.3-p545", "2.1.1"],
+    "3.0.3" => ["1.9.3-p545", "2.1.1"],
+    "3.0.4" => ["1.9.3-p545", "2.1.1"],
+    "3.1.0" => ["1.9.3-p545", "2.1.1"],
+    "3.2.0" => ["1.9.3-p545", "2.1.1"],
+    "3.3.0" => ["1.9.3-p545", "2.1.1"],
+    "3.3.1" => ["1.9.3-p545", "2.1.1"],
+    "3.3.2" => ["1.9.3-p545", "2.1.1"],
+    "3.4.0" => ["1.9.3-p545", "2.1.1"],
+    "3.5.0" => ["1.9.3-p545", "2.1.1"]
+  }
+
+  if version_map[version]
+    return version_map[version]
+  else
+    return [DEFAULT_RUBY_VERSION]
+  end
+end
+
+
+# These combinations will never work, so we list them here
+def skip_combination?(from, to)
+  skip = false
+  # You can only go from 3.5.0 onwards if you first migrate to 3.5.0
+  if (SemVer.parse(from) < SemVer.parse("3.5.0") and
+      SemVer.parse(to) > SemVer.parse("3.5.0"))
+    skip = true
+  end
+  return skip
+end
+
+def attempt_migrations
+  error_messages = []
+  error_count = 0
+
+  versions = get_versions("2.9.12")
+
+  versions.each do |version|
+    # Get all versions higher than the current one
+    target_versions = get_versions(version)
+    $logger.info "---> Will try to migrate from #{version} to #{target_versions.join(", ")}"
+    target_versions.each do |target_version|
+      if skip_combination?(version, target_version)
+        $logger.info "Skipping #{version} to #{target_version} because we know it won't work."
+        next
+      end
+      ruby_versions = lookup_ruby_versions_for(target_version)
+      ruby_versions.each do |ruby_version|
+        $logger.info "Attempting migration from #{version} to #{target_version} using Ruby #{ruby_version}."
+        if attempt_migration(:from => version, :to => target_version, :ruby_version => ruby_version) == true
+          $logger.info "Migration from #{version} to #{target_version} using Ruby #{ruby_version} was successful."
+        else
+          error_message = "Migration from #{version} to #{target_version} using Ruby #{ruby_version} has failed."
+          error_messages << error_message
+          $logger.error error_message
+          error_count += 1
+        end
+      end
+    end
+  end
+
+  if error_count == 0
+    exit 0
+  else
+    $logger.error "Migrations with errors:"
+    $logger.error error_messages.join("\n")
+
+    puts "Migrations with errors:"
+    puts error_messages.join("\n")
+    exit 1
+  end
+end
+
+setup_target_directory
+attempt_migrations
