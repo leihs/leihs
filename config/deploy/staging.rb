a30c0afa1f5e534e0977b50b6616b7bc6c65c7af
Ramón Cahenzli <rca@psy-q.ch>
2014-06-02 16:03:58 +0200

    Link to New Relic config file.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -12,6 +12,7 @@ set :deploy_via, :remote_cache
 
 set :db_config, "/home/leihs/#{application}/database.yml"
 set :ldap_config, "/home/leihs/#{application}/LDAP.yml"
+set :newrelic_config, "/home/leihs/#{application}/newrelic.yml"
 set :secret_token, "/home/leihs/#{application}/secret_token.rb"
 
 set :use_sudo, false
