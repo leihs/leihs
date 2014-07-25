9f666f9545456cc3c701973158815bf7d28b9a42
Ramón Cahenzli <rca@psy-q.ch>
2014-07-25 15:45:17 +0200

    Trying to make sure the rspec tests have a database when starting.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,7 +34,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  # config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
