8cd647ebdc8475724fe57ae143380c7e501f688f
Matus Kmit <matus.kmit@zhdk.ch>
2014-08-07 16:21:30 +0200

    [FIX CI] attachment/image factory

--- factories/attachment_factory.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-
-  factory :attachment do
-
-    filename { Faker::Lorem.word }
-    content_type "image/jpeg"
-    base64_string { Base64.encode64 File.open("features/data/images/image1.jpg").read }
-  end
-end
--- factories/image_factory.rb
+++ factories/image_factory.rb
@@ -1,13 +1,20 @@
 FactoryGirl.define do
 
-  factory :image do
-
-    filename { Faker::Name.name }
+  trait :shared_attachment_attributes do
+    filename { Faker::Lorem.word }
     content_type "image/jpeg"
     base64_string { Base64.encode64 File.open("features/data/images/image1.jpg").read }
+  end
+
+  factory :attachment do
+    shared_attachment_attributes
+  end
+
+  factory :image do
+    shared_attachment_attributes
 
     trait :another do
-      base64_string { Base64.encode64 File.open("features/data/images/image2.jpg").read }      
+      base64_string { Base64.encode64 File.open("features/data/images/image2.jpg").read }
     end
   end
 end
