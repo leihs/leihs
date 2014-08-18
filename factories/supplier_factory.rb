e770645042788f8893f801a0f870c3138f877b3d
Franco Sellitto <franco.sellitto@zhdk.ch>
2014-08-18 17:14:56 +0200

    ensure factory uniqueness

--- factories/supplier_factory.rb
+++ factories/supplier_factory.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
 
   factory :supplier do
-    name { Faker::Lorem.words(4).join }
+    name {
+      begin
+        r = Faker::Lorem.words(rand(2..5)).join
+      end while Supplier.find_by_name(r)
+      r
+    }
   end
 
 end
