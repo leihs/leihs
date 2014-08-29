f1614697c2d77135099dda2cbda6abeef0e942d6
Ramón Cahenzli <rca@psy-q.ch>
2014-08-29 13:38:46 +0200

    Small import helper to import options from a CSV file.

---
+++ doc/import_export/csv_import_of_options.rb
@@ -0,0 +1,24 @@
+
+
+
+require 'pry'
+ip = InventoryPool.find(169)
+log = File.open("/tmp/optionen.log", "a+")
+
+CSV.open("/tmp/optionen.csv", "r", { :col_sep => "\t", :quote_char => "\"", :headers => true}).each do |csv|
+  if csv['price']
+    price = BigDecimal.new(csv['price'])
+  else
+    price = 0.0
+  end
+
+  option = ip.options.find_or_create_by(:inventory_code => csv["inventory_code"], :product => csv['model_name'], :price => price)
+  if option
+    log.puts "Success: #{csv['inventory_code']}"
+  else
+    log.puts "Error: #{csv['inventory_code']}: #{option.errors.full_messages}"
+  end
+
+end
+
+log.close
