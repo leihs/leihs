50aba824940df2f3bb6a3fe2e47fe78b3ad7e520
Ramón Cahenzli <rca@psy-q.ch>
2014-09-11 14:56:46 +0200

    Safer test execution on Travis CI.

--- script/run_nojs_tests.sh
+++ script/run_nojs_tests.sh
@@ -4,9 +4,14 @@ bash script/validate_gettext_files.sh && \
 bundle exec rake leihs:reset && \
 bundle exec cucumber -t ~@javascript features/
 
-if [[ -f tmp/rerun.txt && -s tmp/rerun.txt && $? -ne 0 ]]; then
-  echo "Rerun necessary, the first execution exited with non-0."
-  bundle exec cucumber -t ~@javascript -p rerun
-fi
 
-exit $?
+if [[ $? -ne 0 ]]; then
+  if [[ -f tmp/rerun.txt && -s tmp/rerun.txt ]]; then
+    echo "Rerun necessary, the first execution exited with non-0."
+    bundle exec cucumber -t ~@javascript -p rerun
+    exit $?
+  else
+    echo "Something went wrong during test setup or execution before even running Cucumber."
+    exit 1
+  fi
+fi
