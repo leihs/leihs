97b5c672e7a8c573480098c65627865d8e980c81
Ramón Cahenzli <rca@psy-q.ch>
2014-10-23 14:16:48 +0200

    WIP on tree-attachment mode for Cider CI coverage gathering script.

--- script/retrieve_cider_coverage.rb
+++ script/retrieve_cider_coverage.rb
@@ -4,12 +4,34 @@ require 'rest-client'
 require 'json'
 require 'simplecov'
 require 'fileutils'
+require 'optparse'
+require 'pry'
+
+
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: retrieve_cider_coverage.rb [-c SHA] [-e EXECUTION_ID]"
+  options[:commit_sha] = nil
+  options[:execution_id] = nil
+
+  opts.on('-c', '--commit COMMIT_SHA', 'Commit SHA to retrieve coverage for') do |sha|
+    options[:commit_sha] = sha
+  end
+
+  opts.on('-e', '--execution EXECUTION_ID', 'Execution ID to retrieve coverage for') do |exid|
+    options[:execution_id] = exid
+  end
+end.parse!
 
 
 raise "You must run this from inside the Rails root. I can't find an 'app' directory here." unless Dir.exist?(File.join(FileUtils.pwd, "app"))
 
-execution_id = ARGV[0]
-raise "You must give an execution ID as an argument, e.g.: bundle exec ruby script/retrieve_cider_coverage.rb 37b8408a-8e22-465b-9ae9-ba90b58262fd" if execution_id.nil?
+execution_id = options[:execution_id]
+commit_sha = options[:commit_sha]
+
+raise "You must give an execution ID or a commit SHA, I don't know what the hell to retrieve coverage for without this.\n\nUsage: [-c commit_sha] [-e execution_id]" if execution_id.nil? and commit_sha.nil?
+
+
 
 username = ENV['CIDER_USERNAME']
 password = ENV['CIDER_PASSWORD']
@@ -17,25 +39,40 @@ password = ENV['CIDER_PASSWORD']
 raise "Please set CIDER_USERNAME and CIDER_PASSWORD." if username.nil? or password.nil?
 
 class CiderClient
-  attr_accessor :execution_id
+  attr_accessor :execution_id, :commit_sha
   attr_writer :username, :password
 
   @execution_id = nil
+  @commit_sha = nil
   @username = nil
   @password = nil
+
+
+  def mode
+    if @execution_id and not @commit_sha
+      mode = "execution_id"
+    elsif @commit_sha and not @execution_id
+      mode = "commit_sha"
+    end
+    mode
+  end
 
   def base_url
     return "http://#{@username}:#{@password}@ci2.zhdk.ch"
   end
 
-  # URL starting from the execution, with the passed path appended
-  def entry_url(path)
-    return "#{base_url}/cider-ci/api/v1/execution/#{@execution_id}/#{path}"
-  end
-
   # URL starting from the base URL root, with the passed path appended
   def url(path)
     return "#{base_url}/#{path}"
+  end
+
+  def api_url(path)
+    return url("/cider-ci/api/v1/#{path}")
+  end
+
+  # URL starting from the execution, with the passed path appended
+  def execution_url(path)
+    return api_url("execution/#{@execution_id}/#{path}")
   end
 
   def recurse_tasks(tasks, data)
@@ -52,7 +89,7 @@ class CiderClient
 
   def tasks
     tasks = []
-    tasks = recurse_tasks(tasks, JSON.parse(RestClient.get(entry_url("tasks"))))
+    tasks = recurse_tasks(tasks, JSON.parse(RestClient.get(execution_url("tasks"))))
   end
 
   def trials
@@ -68,6 +105,33 @@ class CiderClient
       end
     end
     trials
+  end
+
+  def tree_id_from_commit(commit_sha)
+    `git show #{commit_sha} --format=%T | head -1`.chomp
+  end
+
+  def tree_attachment_groups
+    tree_id = tree_id_from_commit(self.commit_sha)
+    tree_attachment_groups = []
+    tree_attachments = JSON.parse(RestClient.get(api_url("tree-attachments/#{tree_id}")))
+    binding.pry
+    puts "foo"
+
+  end
+
+  def tree_attachment_hrefs(pattern = /.*/)
+    matching_tas = []
+    tree_attachment_groups.each do |ta|
+      tree_attachment_url = url(ta["href"])
+      tree_attachment_details = JSON.parse(RestClient.get(tree_attachment_url))
+      matching_tas << tree_attachment_details["_links"]["cici:tree-attachment"].select {|ta|
+        ta if ta["href"].match(pattern)
+      }
+    end
+    matching_tas.flatten.map {|ta|
+      ta["href"]
+    }
   end
 
   def trial_attachment_groups
@@ -148,15 +212,25 @@ def merge_results(results)
 end
 
 cc = CiderClient.new
+binding.pry
 cc.username = username
 cc.password = password
+cc.commit_sha = commit_sha
 cc.execution_id = execution_id
 
 resultsets = []
-puts "Gathering coverage data for execution #{cc.execution_id}."
-cc.trial_attachment_hrefs(/.*resultset\.json$/).each do |tah|
-  puts "Gathering results from #{tah}"
-  resultsets << SimpleCov::JSON.parse(cc.trial_attachment_data(tah))
+
+if cc.mode == "execution_id"
+  puts "Gathering coverage data for execution #{cc.execution_id}."
+  cc.trial_attachment_hrefs(/.*resultset\.json$/).each do |tah|
+    puts "Gathering results from #{tah}"
+    resultsets << SimpleCov::JSON.parse(cc.trial_attachment_data(tah))
+  end
+elsif cc.mode == "commit_sha"
+  cc.tree_attachment_hrefs(/.*resultset\.json$/).each do |tah|
+    puts "Gathering results from #{tah}"
+    resultsets << SimpleCov::JSON.parse(cc.tree_attachment_data(tah))
+  end
 end
 
 results = fix_resultsets(resultsets)
