3424fec296317451aad2594e2fef0cf95f91e854
Franco Sellitto <franco.sellitto@zhdk.ch>
2015-05-06 15:06:01 +0200

    [#92606664] model statistics

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -5,6 +5,7 @@ class Admin::StatisticsController < Admin::ApplicationController
                    'Which inventory pool is busiest?',
                    'Who bought the most items?',
                    'Which item is busiest?',
+                   'Which model is busiest?',
                    'Which inventory pool has the most contracts?']
 
     params[:start_date] ||= I18n.l 30.days.ago.to_date
@@ -24,6 +25,8 @@ class Admin::StatisticsController < Admin::ApplicationController
                 Statistics::Base.item_values([InventoryPool, Model], params.to_hash)
               when 'Which item is busiest?'.parameterize
                 Statistics::Base.hand_overs([Item, User], params.to_hash)
+              when 'Which model is busiest?'.parameterize
+                Statistics::Base.hand_overs([Model, Item], params.to_hash)
               when 'Which inventory pool has the most contracts?'.parameterize
                 Statistics::Base.contracts([InventoryPool, User], params.to_hash)
               else
