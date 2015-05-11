cae227065dc1cd33bd90ea122475b994052a62d4
Franco Sellitto <franco.sellitto@zhdk.ch>
2015-05-11 17:04:47 +0200

    [#94268314] prevents wrong database encoding through a config initializer (even for columns

--- config/initializers/database_check.rb
+++ config/initializers/database_check.rb
@@ -19,11 +19,16 @@ if query1.to_h[character_set_key] != character_set_value or query2.to_h[collatio
   if query2.to_h[collation_key] != collation_value
     puts "The MySQL setting for %s was wrong, it was set to %s, now it is set to %s" % [collation_key, query2.to_h[collation_key], collation_value]
   end
+
+  # are the encodings for the existing tables correct?
+  only_tables_no_views = connection.execute("SHOW FULL TABLES WHERE Table_type = 'BASE TABLE'").to_h.keys
+  only_tables_no_views.each do |table_name|
+    connection.execute %Q(ALTER TABLE #{table_name} DEFAULT CHARACTER SET #{character_set_value} COLLATE #{collation_value};)
+
+    connection.columns(table_name).select{|column| not column.collation.nil? and column.collation != 'utf8_general_ci' }.each do |column|
+      connection.execute %Q(ALTER TABLE #{table_name} MODIFY #{column.name} #{column.sql_type} CHARACTER SET #{character_set_value} COLLATE #{collation_value};)
+    end
+  end
 end
 
-# are the encodings for the existing tables correct?
-only_tables_no_views = connection.execute("SHOW FULL TABLES WHERE Table_type = 'BASE TABLE'").to_h.keys
-only_tables_no_views.each do |table_name|
-  connection.execute %Q(ALTER TABLE #{table_name} DEFAULT CHARACTER SET #{character_set_value} COLLATE #{collation_value};)
-end
 
