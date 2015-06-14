72b47b2b58ff717d35ce8f5b7de70cb3bbcbbb5f
Franco Sellitto <franco.sellitto@zhdk.ch>
2015-06-14 20:14:38 +0200

    initialize on conditions

--- config/initializers/audited.rb
+++ config/initializers/audited.rb
@@ -1,10 +1,12 @@
-class ActiveRecord::Base
-  class << self
-    def inherited_with_auditing(subclass)
-      inherited_without_auditing(subclass)
-      subclass.class_eval { audited }
-    end
+if ActiveRecord::Base.connection.tables.include?('audits')
+  class ActiveRecord::Base
+    class << self
+      def inherited_with_auditing(subclass)
+        inherited_without_auditing(subclass)
+        subclass.class_eval { audited }
+      end
 
-    alias_method_chain :inherited, :auditing
+      alias_method_chain :inherited, :auditing
+    end
   end
 end
