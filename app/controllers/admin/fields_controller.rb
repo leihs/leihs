b715b53ba8cad28fb6157eddd212f0b0b558a284
Franco Sellitto <franco.sellitto@zhdk.ch>
2015-06-29 15:27:06 +0200

    [#97006856] validating field attribute

--- app/controllers/admin/fields_controller.rb
+++ app/controllers/admin/fields_controller.rb
@@ -7,13 +7,29 @@ class Admin::FieldsController < Admin::ApplicationController
   end
 
   def update
+    def check_attribute(attr, item)
+      if attr.is_a?(Array)
+        if attr.first == 'properties'
+          # do nothing
+        else
+          target = item.send "build_#{attr.first}"
+          redirect_to :back, flash: {error: _("Unknown attribute %s") % attr.join('.')} and return false unless target.respond_to? "#{attr.last}="
+        end
+      else
+        redirect_to :back, flash: {error: _("Unknown attribute %s") % attr} and return false unless item.respond_to? "#{attr}="
+      end
+      true
+    end
+
     new_fields = params[:fields].delete(:_new_fields_)
+    item = Item.new
 
     i = 0
     params[:fields].each_pair do |field_id, param|
       i += 1
       field = Field.unscoped.find(field_id)
       data = JSON.parse(param[:data]) # TODO validate parsed json
+      return unless check_attribute(data['attribute'], item)
       field.update_attributes(data: data, active: param[:active] == "1", position: i)
     end
 
@@ -21,6 +37,7 @@ class Admin::FieldsController < Admin::ApplicationController
       Field.create do |r|
         r.id = param[:id]
         r.data = JSON.parse(param[:data]) # TODO validate parsed json
+        return unless check_attribute(r.data['attribute'], item)
         r.active = param[:active] == "1"
         r.position = i
       end
