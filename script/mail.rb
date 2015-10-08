2e02e56b9cbe230ffc7e7e428fdfb1799de43218
Ramón Cahenzli <rca@psy-q.ch>
2015-10-08 15:15:06 +0200

    Simple generic command-line mailer.
    Squashed commit of the following:
    
    commit efadf58dd671e56aa61b733c1f29976be7291ad0
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu Oct 8 15:14:39 2015 +0200
    
        Simple mailer that uses the instance's mail configuration.
    
    commit fea7faf54bd53199fcc726a3bdb20d791b8de8ce
    Author: Ramón Cahenzli <rca@psy-q.ch>
    Date:   Thu Oct 8 14:49:26 2015 +0200
    
        Non-working mailer.

---
+++ script/mail.rb
@@ -0,0 +1,41 @@
+require 'optparse'
+
+class Mailer < ActionMailer::Base
+  # Don't call this method "message", that's a reserved word
+  def generic_message(recipients, subject, body)
+      mail(to: recipients,
+           from: Setting.default_email,
+           subject: subject,
+           body: body)
+  end
+end
+
+options = OpenStruct.new
+options.recipients = ''
+options.subject = 'No subject'
+options.body = 'No message.'
+
+OptionParser.new do |opts|
+  opts.banner = 'Usage: rails runner mail.rb [options]'
+
+  opts.on('-r', '--recipients RECIPIENTS', Array, 'The recipients of the email, comma-separated, no spaces') do |r|
+    options.recipients = r
+  end
+
+  opts.on('-s', '--subject [SUBJECT]', 'The subject of the email') do |s|
+    options.subject = s
+  end
+
+  opts.on('-b', '--body [BODY]', 'The body of the email') do |b|
+    options.body = b
+  end
+end.parse!
+
+message = Mailer.generic_message(options.recipients, options.subject, options.body)
+if message.deliver_now
+  puts "Sent email to #{options.recipients.join(',')}."
+  exit 0
+else
+  $stderr.puts "Could not send email to #{options.recipients.join(',')}."
+  exit 1
+end
