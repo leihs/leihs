3211d24a0d7b2a7036e72c704569514121104754
Franco Sellitto <franco.sellitto@zhdk.ch>
2015-12-15 12:53:48 +0100

    remove ZHdK custom fields from initializer

--- config/initializers/fields.rb
+++ config/initializers/fields.rb
@@ -288,64 +288,6 @@ if ActiveRecord::Base.connection.tables.include?('fields')
           target_type: 'item',
           group: 'Invoice Information'
       }, {
-          id: :properties_umzug,
-          label: 'Umzug',
-          attribute: ['properties', 'umzug'],
-          type: 'select',
-          target_type: 'item',
-          values: [{label: 'zügeln', value: 'zügeln'}, {label: 'sofort entsorgen', value: 'sofort entsorgen'}, {label: 'bei Umzug entsorgen', value: 'bei Umzug entsorgen'}, {label: 'bei Umzug verkaufen', value: 'bei Umzug verkaufen'}],
-          default: 'zügeln',
-          permissions: {role: :inventory_manager, owner: true},
-          group: 'Umzug'
-      }, {
-          id: :properties_zielraum,
-          label: 'Zielraum',
-          attribute: ['properties', 'zielraum'],
-          type: 'text',
-          target_type: 'item',
-          permissions: {role: :inventory_manager, owner: true},
-          group: 'Umzug'
-      }, {
-          id: :properties_ankunftsdatum,
-          label: 'Ankunftsdatum',
-          attribute: ['properties', 'ankunftsdatum'],
-          type: 'date',
-          target_type: 'item',
-          permissions: {role: :inventory_manager, owner: true},
-          group: 'Toni Ankunftskontrolle'
-      }, {
-          id: :properties_ankunftszustand,
-          label: 'Ankunftszustand',
-          attribute: ['properties', 'ankunftszustand'],
-          type: 'select',
-          target_type: 'item',
-          values: [{label: 'intakt', value: 'intakt'}, {label: 'transportschaden', value: 'transportschaden'}],
-          default: 'intakt',
-          permissions: {role: :inventory_manager, owner: true},
-          group: 'Toni Ankunftskontrolle'
-      }, {
-          id: :properties_ankunftsnotiz,
-          label: 'Ankunftsnotiz',
-          attribute: ['properties', 'ankunftsnotiz'],
-          type: 'textarea',
-          target_type: 'item',
-          permissions: {role: :inventory_manager, owner: true},
-          group: 'Toni Ankunftskontrolle'
-      }, {
-          id: :properties_anschaffungskategorie,
-          label: 'Anschaffungskategorie',
-          attribute: ['properties', 'anschaffungskategorie'],
-          value_label: ['properties', 'anschaffungskategorie'],
-          required: true,
-          type: 'select',
-          target_type: 'item',
-          values: [{label: '', value: nil}, {label: 'Werkstatt-Technik', value: 'Werkstatt-Technik'}, {label: 'Produktionstechnik', value: 'Produktionstechnik'}, {label: 'AV-Technik', value: 'AV-Technik'}, {label: 'Musikinstrumente', value: 'Musikinstrumente'}, {label: 'Facility Management', value: 'Facility Management'}, {label: 'IC-Technik/Software', value: 'IC-Technik/Software'}],
-          default: nil,
-          visibility_dependency_field_id: :is_inventory_relevant,
-          visibility_dependency_value: 'true',
-          permissions: {role: :inventory_manager, owner: true},
-          group: 'Inventory'
-      }, {
           id: :properties_activation_type,
           label: 'Activation Type',
           attribute: ['properties', 'activation_type'],
