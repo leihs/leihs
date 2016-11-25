a3439257e1784371f8037a1bf49f6aa19c756d4e
Thomas Schank <DrTom@schank.ch>
2016-11-25 12:16:25 +0100

    Add RDBMS vendor independent data exporter

---
+++ lib/leihs/dbio.rb
@@ -0,0 +1,33 @@
+module Leihs
+  module DBIO
+    class << self
+
+      TABLES = ActiveRecord::Base.connection.tables
+
+      def reload!
+        load File.absolute_path(__FILE__)
+      end
+
+      def rows(table)
+        class_name = "LeihsDBIO#{table.to_s.capitalize}"
+        eval <<-RB.strip_heredoc
+          class ::#{class_name} < ActiveRecord::Base
+            self.table_name = '#{table}'
+          end
+        RB
+        class_name.constantize.all.map(&:attributes)
+      end
+
+      def data
+        TABLES.map do |table|
+          [table, (rows table)]
+        end.to_h
+      end
+
+      def export(filename = Rails.root.join('tmp', 'db_data.yml'))
+        ::IO.write filename, data.to_yaml
+      end
+    end
+
+  end
+end
