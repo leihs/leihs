22e35e43110866543042b1ac2d9c3996a150b62f
uvb <urs.vanbinsbergen@zhdk.ch>
2023-02-08 16:51:33 +0100

    release: 6.9.0-RC.1

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -exu
 
-export DEV_INITIALS=mk
-export RELEASE_MAJOR_MINOR=6.6
+export DEV_INITIALS=uvb
+export RELEASE_MAJOR_MINOR=6.9
 export RELEASE_PATCH=0
-export RELEASE_PRE='-RC.2' # or '' for stable release
+export RELEASE_PRE='-RC.1' # or '' for stable release
 export VERSION_PREFIX='' # for madek its 'v' 
 
 export RELEASE_MAIN="${RELEASE_MAJOR_MINOR}.${RELEASE_PATCH}"
