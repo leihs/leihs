7944d9755122aaf8dd6734bab61df26b87082728
Thomas Schank <DrTom@schank.ch>
2023-02-09 10:07:34 +0100

    Fix procure (build) and deploy

--- bin/git-check-ahead-of-origin-master.rb
+++ bin/git-check-ahead-of-origin-master.rb
@@ -11,66 +11,50 @@ end
 
 require 'active_support/all'
 
-ALL_REPOS= `git submodule status --recursive`.split("\n").map{|sub| sub.strip.split(/\s+/).map(&:strip)}.map(&:second).append(".").sort()
 
+############## options ########################################################
+require 'optparse'
+
+options = {skip_fetch: false}
+OptionParser.new do |parser|
+  parser.banner = "git-check-ahead-of-origin-master [options]"
+  parser.on("-k", "--skip-fetch", "Skip fetching origin/master") do |s|
+    options[:skip_fetch] = s
+  end
+  parser.on("-h", "--help") do 
+    puts parser 
+    exit 0
+  end
+end.parse!
+
+
+############## main ###########################################################
+
+
+ALL_REPOS= `git submodule status --recursive`.split("\n").map{|sub| sub.strip.split(/\s+/).map(&:strip)}.map(&:second).append(".").sort()
 BEHIND_AHEAD= ALL_REPOS.map { |repo| 
-  `git -C #{repo} fetch origin master`
+  `git -C #{repo} fetch origin master` unless options[:skip_fetch]
   [repo, `git -C #{repo} rev-list --left-right --count origin/master...HEAD` \
     .split(/\s/).map(&:to_i)].flatten
 }
 
 $exit_val = 0
 
-print("###############################################################################\n")
-print("behind ahead repo\n")
-BEHIND_AHEAD.each do |repo, behind, ahead|
-  printf("   %3d   %3d %s\n", behind, ahead, repo )
-  $exit_val += (behind == 0 ? 0 : 1)
-end
-print("behind ahead repo\n")
-print("###############################################################################\n")
-if $exit_val == 0
-  puts "All OK"
-else
-  puts "ERROR: no HEAD must be behind master <=> behind values must all be zero"
-end
-
-
-exit $exit_val
-
-
-
-SHARED_SUBS_MATCHERS= 
-  {database: /database$/,
-   legacy: /legacy$|legacy-api$/,
-   ui: /leihs-ui$/,
-   clj: /shared-clj$/}
-
-SHARED_SUBS = SHARED_SUBS_MATCHERS.map{ |key,match|
-  [key, ALL_SUBS.filter{|sub| sub.second[match]}]
-}.to_h
-
-# print
-SHARED_SUBS.each do |name, subs|
-  printf("SUBMODULE %s\n", name)
-  subs.each do | id,sub |
-    printf(" %s %s\n",id, sub)
+def main 
+  print("###############################################################################\n")
+  print("behind ahead repo\n")
+  BEHIND_AHEAD.each do |repo, behind, ahead|
+    printf("   %3d   %3d %s\n", behind, ahead, repo )
+    $exit_val += (behind == 0 ? 0 : 1)
   end
-end
-
-puts ""
-
-$exit_val = 0
-
-# check
-SHARED_SUBS.each do |name, subs|
-  ids = Set.new(subs.map(&:first))
-  if ids.size != 1 
-    printf("ERROR %s : %s \n", name, ids.join(", ")) 
-    $exit_val += 1
+  print("behind ahead repo\n")
+  print("###############################################################################\n")
+  if $exit_val == 0
+    puts "All OK"
+  else
+    puts "ERROR: no HEAD must be behind master <=> behind values must all be zero"
   end
+  exit $exit_val
 end
 
-puts "ALL OK" if $exit_val == 0 
-
-exit $exit_val
+main()
