a6ba953db1ff37c2dcad927db78ca5c04f601b44
Matus Kmit <matuskmit1@gmail.com>
2023-03-06 16:34:46 +0530

    release: 6.9.1

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -3,7 +3,7 @@
 export DEV_INITIALS=mk
 export RELEASE_MAJOR_MINOR=6.9
 export RELEASE_PATCH=1
-export RELEASE_PRE='-RC.1' # e.g. '-RC.1'; or '' for stable release
+export RELEASE_PRE='' # e.g. '-RC.1'; or '' for stable release
 export VERSION_PREFIX='' # for madek its 'v' 
 
 export RELEASE_MAIN="${RELEASE_MAJOR_MINOR}.${RELEASE_PATCH}"
