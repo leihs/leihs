11e974e505b447c852b939e5383a2d03ec69a10d
Matus Kmit <matuskmit1@gmail.com>
2023-08-22 16:49:40 +0200

    release: 7.0.1-RC.2

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -3,7 +3,7 @@
 export DEV_INITIALS=mk
 export RELEASE_MAJOR_MINOR=7.0
 export RELEASE_PATCH=1
-export RELEASE_PRE='-RC.1' # e.g. '-RC.1'; or '' for stable release
+export RELEASE_PRE='-RC.2' # e.g. '-RC.1'; or '' for stable release
 export VERSION_PREFIX='' # for madek its 'v' 
 
 export RELEASE_MAIN="${RELEASE_MAJOR_MINOR}.${RELEASE_PATCH}"
