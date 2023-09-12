73c790735d61a986f9b9d7cee51e22f40885498b
Matus Kmit <matuskmit1@gmail.com>
2023-09-12 18:23:12 +0200

    chore: sync release info

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -exu
 
 export DEV_INITIALS=mk
-export RELEASE_MAJOR_MINOR=7.0
-export RELEASE_PATCH=1
+export RELEASE_MAJOR_MINOR=7.1
+export RELEASE_PATCH=0
 export RELEASE_PRE='' # e.g. '-RC.1'; or '' for stable release
 export VERSION_PREFIX='' # for madek its 'v' 
 
