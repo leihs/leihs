28b821fe3b48f2cd4b4aad5e81118efec0944d1a
Matus Kmit <matuskmit1@gmail.com>
2023-12-15 15:03:24 +0100

    chore: sync release info

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -exu
 
-export DEV_INITIALS=jp
-export RELEASE_MAJOR_MINOR=7.2
+export DEV_INITIALS=mk
+export RELEASE_MAJOR_MINOR=7.3
 export RELEASE_PATCH=0
 export RELEASE_PRE='' # e.g. '-RC.1'; or '' for stable release
-export VERSION_PREFIX='' # for madek its 'v' 
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
 export RELEASE="$RELEASE_MAIN$RELEASE_PRE"
-export RELEASE_NAME="$VERSION_PREFIX$RELEASE"
+export RELEASE_NAME="$RELEASE"
+export LEIHS_REPO=$(pwd)
 
-echo $RELEASE_MAIN
-echo $RELEASE
-echo $RELEASE_NAME
+echo "RELEASE_MAIN: $RELEASE_MAIN"
+echo "RELEASE: $RELEASE"
+echo "RELEASE_NAME: $RELEASE_NAME"
+echo "LEIHS_REPO: $LEIHS_REPO" # should be the path to your cloned leihs repository
