3d8225a538e7ca8f04d437b0ea6615ba2dbaa521
mradl <manuel.radl@zhdk.ch>
2024-03-07 19:02:56 +0100

    chore: sync release info

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -exu
 
-export DEV_INITIALS=uvb
-export RELEASE_MAJOR_MINOR=7.3
-export RELEASE_PATCH=2
-export RELEASE_PRE='' # e.g. '-RC.1'; or '' for stable release
+export DEV_INITIALS=mr
+export RELEASE_MAJOR_MINOR=7.4
+export RELEASE_PATCH=0
+export RELEASE_PRE='-RC.1' # e.g. '-RC.1'; or '' for stable release
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
 export RELEASE="$RELEASE_MAIN$RELEASE_PRE"
@@ -14,3 +14,5 @@ echo "RELEASE_MAIN: $RELEASE_MAIN"
 echo "RELEASE: $RELEASE"
 echo "RELEASE_NAME: $RELEASE_NAME"
 echo "LEIHS_REPO: $LEIHS_REPO" # should be the path to your cloned leihs repository
+
+echo "\nPERSONAL-BRANCH: $DEV_INITIALS/v/$RELEASE_MAJOR_MINOR-staging"
