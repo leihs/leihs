57182775de3f2a54dd37f1d805d5025f6a279c06
Matus Kmit <matuskmit1@gmail.com>
2024-04-17 12:35:23 +0200

    chore: sync release info

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -exu
 
-export DEV_INITIALS=mr
-export RELEASE_MAJOR_MINOR=7.4
+export DEV_INITIALS=mk
+export RELEASE_MAJOR_MINOR=7.5
 export RELEASE_PATCH=0
-export RELEASE_PRE='-RC.1' # e.g. '-RC.1'; or '' for stable release
+export RELEASE_PRE='' # e.g. '-RC.1'; or '' for stable release
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
 export RELEASE="$RELEASE_MAIN$RELEASE_PRE"
@@ -15,4 +15,4 @@ echo "RELEASE: $RELEASE"
 echo "RELEASE_NAME: $RELEASE_NAME"
 echo "LEIHS_REPO: $LEIHS_REPO" # should be the path to your cloned leihs repository
 
-echo "\nPERSONAL-BRANCH: $DEV_INITIALS/v/$RELEASE_MAJOR_MINOR-staging"
+echo "PERSONAL-BRANCH: $DEV_INITIALS/v/$RELEASE_MAJOR_MINOR-staging"
