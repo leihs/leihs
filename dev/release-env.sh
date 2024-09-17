929c7169f0b1dd7064dbbfa48aab16bfe91f41fb
Jan Pistor <pistor.jan@gmail.com>
2024-09-17 15:32:28 +0200

    chore: sync release info

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -exu
 
-export DEV_INITIALS=mk
+export DEV_INITIALS=jp
 export RELEASE_MAJOR_MINOR=7.7
-export RELEASE_PATCH=0
+export RELEASE_PATCH=1
 export RELEASE_PRE='' # e.g. '-RC.1'; or '' for stable release
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
