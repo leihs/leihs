d74d8fae23945576758ee1a5f16f7dd94649e0c7
Matus Kmit <matuskmit1@gmail.com>
2025-05-14 16:08:12 +0200

    release: 7.10.0-RC.3

--- dev/release-env.sh
+++ dev/release-env.sh
@@ -3,7 +3,7 @@
 export DEV_INITIALS=mk
 export RELEASE_MAJOR_MINOR=7.10
 export RELEASE_PATCH=0
-export RELEASE_PRE='-RC.2' # e.g. '-RC.1'; or '' for stable release
+export RELEASE_PRE='-RC.3' # e.g. '-RC.1'; or '' for stable release
 
 export RELEASE_MAIN="$RELEASE_MAJOR_MINOR.$RELEASE_PATCH"
 export RELEASE="$RELEASE_MAIN$RELEASE_PRE"
